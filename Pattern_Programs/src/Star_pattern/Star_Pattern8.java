753afed2ed52ad3e3705d80c78ba7f3aa9b28758
AKASH KUMAR <akakash201@gmail.com>
2025-01-24 23:15:55 +0530

    .

---
+++ Pattern_Programs/src/Star_pattern/Star_Pattern8.java
@@ -0,0 +1,44 @@
+package Star_pattern;
+
+//   * 
+//  * * 
+// * * * 
+//* * * * 
+
+import java.util.Scanner;
+
+public class Star_Pattern8 
+{
+	public static void print(int n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=n-1;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		while(true)
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the value of n: ");	
+			int n=sc.nextInt();
+			print(n);
+		}
+
+	}
+
+}
