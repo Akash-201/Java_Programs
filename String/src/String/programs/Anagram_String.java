64bee0fce81835856ed8ecba968487a57c128375
AKASH KUMAR <akakash201@gmail.com>
2025-02-03 21:53:36 +0530

    .

---
+++ String/src/String/programs/Anagram_String.java
@@ -0,0 +1,34 @@
+package String.programs;
+
+import java.util.Arrays;
+
+public class Anagram_String
+{
+	public static boolean check(String str1,String str2)
+	{
+		if(str1.length()!=str2.length())
+		{
+			return false;
+		}
+		char[] ch1=str1.toCharArray();
+		char[] ch2=str2.toCharArray();
+		
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+		
+		str1=new String(ch1);
+		str2=new String(ch2);
+		
+		return str1.equals(str2);
+				
+	}
+	
+	public static void main(String[] args) 
+	{
+		String str1="cbba";
+		String str2="abcb";
+		System.out.println(check(str1,str2));
+		
+	}
+
+}
