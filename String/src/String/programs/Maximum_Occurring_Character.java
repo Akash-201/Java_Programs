28d71fa3c0876c6fa11f290eabf9e7cb9b87e738
AKASH KUMAR <akakash201@gmail.com>
2025-02-05 16:46:36 +0530

    .

---
+++ String/src/String/programs/Maximum_Occurring_Character.java
@@ -0,0 +1,51 @@
+package String.programs;
+
+
+//input: aabbbcccc
+//output: c
+//because 'c' appears 4 times, which is more than 'a' (2 times) or 'b' (3 times).
+
+public class Maximum_Occurring_Character 
+{
+	
+	public static void check(String str)
+	{
+		char[] ch=str.toCharArray();
+		
+		int maxCount=0;
+		
+		char maxChar=' ';
+		
+		
+		for(int i=0;i<ch.length;i++)
+		{
+			if(ch[i]== ' ')
+				continue;
+			
+			int count=1;
+			for(int j=0;j<ch.length;j++)
+			{
+				if(ch[i]==ch[j] && i!=j) 
+				{
+					count++;
+					ch[j]=' ';
+				}
+			}
+			if(count>maxCount)
+			{
+				maxCount=count;
+				maxChar=ch[i];
+			}
+		}
+		
+		System.out.println("The character that appears the most is: " + maxChar + " ---> " + maxCount);
+	}
+	public static void main(String[] args) 
+	{
+		String str="aabbbcccc";
+		check(str);
+		
+	}
+	
+
+}
