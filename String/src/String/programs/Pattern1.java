fd1f246d3db6a59e50f9fec71789a2fc098629b9
AKASH KUMAR <akakash201@gmail.com>
2025-02-05 15:35:42 +0530

    .

---
+++ String/src/String/programs/Pattern1.java
@@ -0,0 +1,39 @@
+package String.programs;
+
+// input: aabbbcccc
+// output: a2b3c4
+
+public class Pattern1
+{
+	public static void countPattern(String str)
+	{
+		
+		char[] ch=str.toCharArray();
+		for(int i=0;i<str.length()-1;i++)
+		{
+			if(ch[i]==' ')
+			
+				continue;
+			int count=1;
+			for(int j=i+1;j<str.length();j++)
+			{
+				if(ch[i]==ch[j])
+				{
+					count++;
+					ch[j]=' ';
+				}
+			}
+			
+			System.out.print(ch[i]+""+count);
+			
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		String str="aabbbcccc";
+		countPattern(str);
+	}
+	
+
+}
