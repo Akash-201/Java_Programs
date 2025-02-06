32094e1e7bca3d9d1eb4eb2d0943874dc763e5a4
AKASH KUMAR <akakash201@gmail.com>
2025-02-07 00:42:00 +0530

    .

---
+++ String/src/String/programs/String_To_Integer.java
@@ -0,0 +1,16 @@
+package String.programs;
+
+public class String_To_Integer 
+{
+	public static void main(String[] args) {
+		String str = "5678";
+
+		// Convert string to integer
+		int number = Integer.valueOf(str);
+
+		// Output the result
+		System.out.println("Converted integer: " + number);
+	}
+
+
+}
