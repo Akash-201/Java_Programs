fae9ca0e817458a945970574e6eec13dc3fbef12
AKASH KUMAR <akakash201@gmail.com>
2025-02-11 23:35:58 +0530

    .

---
+++ Collections_Framework/src/ArrayList/Insert_An_Element_At_A_Particular_Position.java
@@ -0,0 +1,39 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+//insert an element at a particular position of an ArrayList
+
+
+public class Insert_An_Element_At_A_Particular_Position
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<String> list=new ArrayList<>();
+
+		list.add("ONE");
+		list.add("TWO");
+		list.add("THREE");
+		list.add("FOUR");
+		list.add("FIVE");
+
+		// print all elements
+		System.out.println(list);
+
+		//Inserting "AAA" at index 1
+		list.add(1,"AAA");		
+
+		System.out.println(list);
+
+
+		//Inserting "AAA" at index 1
+		list.add(3,"BBB");		
+
+		System.out.println(list);
+
+
+
+	}
+
+
+}
