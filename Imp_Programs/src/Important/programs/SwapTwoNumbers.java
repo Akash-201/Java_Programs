ab5a87b738e56803e185fa5d6191e96cafbdffbc
AKASH KUMAR <akakash201@gmail.com>
2025-02-16 22:16:13 +0530

    .

---
+++ Imp_Programs/src/Important/programs/SwapTwoNumbers.java
@@ -0,0 +1,30 @@
+package Important.programs;
+
+import java.util.Scanner;
+
+// without using third variable
+
+public class SwapTwoNumbers
+{
+	public static void swapTwoNumber(int a, int b)
+	{
+		a=a^b;
+		b=a^b;
+		a=a^b;
+
+		System.out.println("After swapping a is: "+a);
+		System.out.println("After swapping b is: "+b);
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		int a=sc.nextInt();
+		System.out.print("Enter second number: ");
+		int b=sc.nextInt();
+		swapTwoNumber(a, b);
+
+	}
+	
+
+}
