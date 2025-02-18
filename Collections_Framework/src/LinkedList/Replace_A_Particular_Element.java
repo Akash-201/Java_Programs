e0fe49855fac6732b7e141cab9f37478a24aa059
AKASH KUMAR <akakash201@gmail.com>
2025-02-18 23:54:22 +0530

    .

--- Collections_Framework/src/LinkedList/Replace_A_Particular_Element.java
+++ Collections_Framework/src/LinkedList/Replace_A_Particular_Element.java
@@ -1,4 +1,7 @@
 package LinkedList;
+//replace an element at a specific position of a LinkedList with the given element
+
+//To replace an element at specific position of a LinkedList , use set() method.
 
 import java.util.LinkedList;
 
@@ -16,7 +19,9 @@ public class Replace_A_Particular_Element
 		// print all elements
 		System.out.println(list);
 		
+		//Replacing an element at a particular index
 		list.set(1,15);
+		list.set(3,45);
 		System.out.println(list);
 	}
 
