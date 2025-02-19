adceaaad7101ca3d682e2d2e5bd43de81b574eb2
AKASH KUMAR <akakash201@gmail.com>
2025-02-19 20:57:15 +0530

    .

---
+++ Collections_Framework/src/LinkedList/Retrieve_Element.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Retrieve_Element 
+{
+	public static void main(String[] args)
+	{
+		LinkedList<Integer> list=new LinkedList<>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+
+		// print all elements
+		System.out.println(list);
+
+		//Retrieving the elements from the head
+		System.out.println(list.getFirst());
+
+
+		//Retrieving the elements from the tail
+		System.out.println(list.getLast());
+	}
+
+}
