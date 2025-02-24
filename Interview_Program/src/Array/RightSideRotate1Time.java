3941745fb4526f183a97cfa0ab1cf5e6e06fe68b
AKASH KUMAR <akakash201@gmail.com>
2025-02-24 23:03:41 +0530

    .

--- Interview_Program/src/Array/RightSideRotate1Time.java
+++ Interview_Program/src/Array/RightSideRotate1Time.java
@@ -8,13 +8,20 @@ public class RightSideRotate1Time
 {
 	public static void rightRotate(int[] arr)
 	{
+		// Store last element
 		int lastIndex=arr[arr.length-1];
+
+		// Shift elements to the right
 		for(int i=arr.length-1;i>0;i--)
 		{
 			arr[i]=arr[i-1];
-			
+
 		}
+
+		// Place last element at first position
 		arr[0]=lastIndex;
+
+		// Print rotated array
 		System.out.println(Arrays.toString(arr));
 	}
 	public static void main(String[] args) {
