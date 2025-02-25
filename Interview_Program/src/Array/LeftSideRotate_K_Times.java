aaa03a03941acdcef58ce6258298acaeafa370c5
AKASH KUMAR <akakash201@gmail.com>
2025-02-25 20:32:46 +0530

    .

---
+++ Interview_Program/src/Array/LeftSideRotate_K_Times.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Arrays;
+
+public class LeftSideRotate_K_Times 
+{
+	public static void leftSideRotate(int[] arr)
+	{
+		int firstIndex=arr[0];
+		
+		for(int i=0;i<arr.length-1;i++)
+		{
+			arr[i]=arr[i+1];
+		}
+		arr[arr.length-1]=firstIndex;
+	}
+	
+	public static void leftRotate_K_Times(int[] arr, int k)
+	{
+		k=k% arr.length;
+		for(int i=1;i<=k;i++)
+		{
+			leftSideRotate(arr);
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4,5};
+		leftRotate_K_Times(arr, 2);
+	}
+	
+
+}
