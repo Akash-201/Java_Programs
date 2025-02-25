eb5c401f774e53bfd85e0b29d2cf194624f46f9e
AKASH KUMAR <akakash201@gmail.com>
2025-02-25 20:36:16 +0530

    .

--- Interview_Program/src/Array/LeftSideRotate_K_Times.java
+++ Interview_Program/src/Array/LeftSideRotate_K_Times.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 
 public class LeftSideRotate_K_Times 
 {
+	// Method to rotate the array left by one position
 	public static void leftSideRotate(int[] arr)
 	{
 		int firstIndex=arr[0];
-		
+
 		for(int i=0;i<arr.length-1;i++)
 		{
 			arr[i]=arr[i+1];
 		}
 		arr[arr.length-1]=firstIndex;
 	}
-	
+
+	// Method to rotate the array left by k times
 	public static void leftRotate_K_Times(int[] arr, int k)
 	{
 		k=k% arr.length;
@@ -24,10 +26,11 @@ public class LeftSideRotate_K_Times
 		}
 		System.out.println(Arrays.toString(arr));
 	}
+
 	public static void main(String[] args) {
 		int[] arr= {1,2,3,4,5};
-		leftRotate_K_Times(arr, 2);
+		leftRotate_K_Times(arr, 3);
 	}
-	
+
 
 }
