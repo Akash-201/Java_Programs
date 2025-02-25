2cdc9af1438357ac420955ed552759ac1cfc9f99
AKASH KUMAR <akakash201@gmail.com>
2025-02-25 20:03:07 +0530

    .

--- Interview_Program/src/Array/LeftSideRotate.java
+++ Interview_Program/src/Array/LeftSideRotate.java
@@ -4,18 +4,21 @@ import java.util.Arrays;
 
 public class LeftSideRotate 
 {
-	 // Method to rotate the array left by one position
+	// Method to rotate the array left by one position
 	public static void leftRotation(int[] arr)
 	{
 		int firstIndex=arr[0];   // Store the first element of the array
 
-		 // Shift all elements to the left by one position
+		// Shift all elements to the left by one position
 		for(int i=0;i<arr.length-1;i++)
 		{
 			arr[i]=arr[i+1];
 
 		}
+
+		// Place the first element at the last position
 		arr[arr.length-1]=firstIndex;
+
 		System.out.println(Arrays.toString(arr));
 	}
 
