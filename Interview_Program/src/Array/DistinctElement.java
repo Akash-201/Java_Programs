939c28a1ab5c4eacefca64d93063de0f6ea1d829
AKASH KUMAR <akakash201@gmail.com>
2025-02-27 20:05:57 +0530

    .

---
+++ Interview_Program/src/Array/DistinctElement.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class DistinctElement 
+{
+	public static void distinctElement(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			int count=1;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					count++;
+					arr[j]=-1;
+				}
+			}
+			if(arr[i]!=-1 && count==1)
+			{
+				System.out.println(arr[i]);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr= {4,6,3,6,6,3,7,9,4};
+		distinctElement(arr);
+	}
+
+}
