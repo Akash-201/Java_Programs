758a50a83e58798ec3d7dd066637c69779dfe5d3
AKASH KUMAR <akakash201@gmail.com>
2025-02-28 22:45:47 +0530

    .

---
+++ Interview_Program/src/Array/DistinctElements.java
@@ -0,0 +1,31 @@
+package Array;
+
+// distinct elements are all different values, regardless of their occurance count
+
+public class DistinctElements
+{
+	public static void distinctElements(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			int count=1;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					count++;
+					arr[j]=-1;
+				}
+			}
+			if(arr[i]!=-1)
+			{
+				System.out.print(arr[i]+" ");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr= {1,4,4,7,1,9,0,8,7,13,9};
+		distinctElements(arr);
+	}
+
+}
