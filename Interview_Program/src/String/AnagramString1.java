56b2d307d8a193185f3a45ba886529a08336f7d6
AKASH KUMAR <akakash201@gmail.com>
2025-03-05 01:02:12 +0530

    .

---
+++ Interview_Program/src/String/AnagramString1.java
@@ -0,0 +1,51 @@
+package String;
+
+import java.util.Arrays;
+
+public class AnagramString1
+{
+	public static boolean checkAnagram(String s1, String s2)
+	{
+		if(s1.length()!=s2.length())
+		{
+			return false;
+		}
+
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+
+		char[] ch1=s1.toCharArray();
+		char[] ch2=s2.toCharArray();
+
+		sortArray(ch1);
+		sortArray(ch2);
+		return Arrays.equals(ch1, ch2);
+
+
+	}
+
+	public static void sortArray(char[] arr)
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					char temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) 
+	{
+		String s1="abhc";
+		String s2="bcaH";
+
+		System.out.println(checkAnagram(s1, s2));
+
+	}
+
+}
