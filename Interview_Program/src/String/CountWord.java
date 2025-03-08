61e9489b6e84a33988a2e406224d5a89df99aa60
AKASH KUMAR <akakash201@gmail.com>
2025-03-09 00:11:29 +0530

    .

--- Interview_Program/src/String/CountWord.java
+++ Interview_Program/src/String/CountWord.java
@@ -4,7 +4,7 @@ public class CountWord
 {
 	public static void countWord(String s1)
 	{
-		String[] s2=s1.split(" ");
+		String[] s2=s1.trim().split("\\s+");  // Splitting using one or more spaces
 		int count=0;
 		for(int i=0;i<s2.length;i++)
 		{
@@ -15,7 +15,7 @@ public class CountWord
 	}
 	public static void main(String[] args)
 	{
-		String s1="Hello World ";
+		String s1="Hello  World";
 		countWord(s1);
 		
 	}
