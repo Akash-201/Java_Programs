f2b7514d36d0639ece74f577f1bcfb6f4c58ca8f
AKASH KUMAR <akakash201@gmail.com>
2025-03-10 21:49:31 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/EvenOdd.java
@@ -0,0 +1,28 @@
+package NumberPrograms;
+
+import java.util.Scanner;
+
+public class EvenOdd 
+{
+	public static void checkEvenOdd(int n)
+	{
+		if(n%2==0)
+		{
+			System.out.println(n+" is a even number");
+		}
+		else {
+			System.out.println(n+" is a odd number");
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+		System.out.print("Enter any number: ");
+		int n=sc.nextInt();
+		checkEvenOdd(n);
+		}
+	}
+
+}
