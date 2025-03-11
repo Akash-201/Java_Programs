bc2db92912b0e1782ea2e523ecf329d8b309a128
AKASH KUMAR <akakash201@gmail.com>
2025-03-11 19:39:06 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package NumberPrograms;
+
+public class FibonacciNumber
+{
+	public static void fibonacciNumber(int n)
+	{
+		int a=0;
+		int b=1;
+		
+		for(int i=0;i<=n;i++)
+		{
+			System.out.print(a+" ");
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		fibonacciNumber(10);
+	}
+
+}
