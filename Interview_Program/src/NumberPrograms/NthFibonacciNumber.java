c9e6ddbbc5bf668bea3b9eca0806b3aff7429df0
AKASH KUMAR <akakash201@gmail.com>
2025-03-12 22:14:01 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/NthFibonacciNumber.java
@@ -0,0 +1,32 @@
+package NumberPrograms;
+
+import java.util.Scanner;
+
+public class NthFibonacciNumber 
+{
+	public static void nthFibonacciNumber(int n)
+	{
+		int a=0;
+		int b=1;
+		for(int i=1;i<=n;i++)
+		{
+			if(i==n)
+			{
+				System.out.println(a);
+			}
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+		System.out.print("Enter number: ");
+		int n=sc.nextInt();
+		nthFibonacciNumber(n);
+		}
+	}
+
+}
