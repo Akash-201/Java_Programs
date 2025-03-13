08de85ea1b372641cd07bb7deeefc2e6d3b72b39
AKASH KUMAR <akakash201@gmail.com>
2025-03-13 16:45:26 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/PalindromeNumber.java
@@ -0,0 +1,37 @@
+package NumberPrograms;
+
+import java.util.Scanner;
+
+public class PalindromeNumber 
+{
+	public static void checkPalindrome(int n)
+	{
+		int temp=n;
+		int rev=0;
+		while(n>0)
+		{
+			int rem=n%10;
+			 rev=rev*10+rem;
+			 n=n/10;
+		}
+		if(temp==rev)
+		{
+			System.out.println(temp+" is a palindrome number.");
+		}
+		else
+		{
+			System.out.println(temp+" is not a palindrome number.");
+		}
+	}
+	public static void main(String[] arge)
+	{
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+			System.out.print("Enter a number: ");
+			int n=sc.nextInt();
+			checkPalindrome(n);
+		}
+	}
+
+}
