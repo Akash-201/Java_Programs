f4cb42139d96a1c581bcde531a01125207303b1b
AKASH KUMAR <akakash201@gmail.com>
2025-03-15 23:00:13 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/SpyNumber.java
@@ -0,0 +1,36 @@
+package NumberPrograms;
+import java.util.Scanner;
+public class SpyNumber 
+{
+	public static void checkSpyNumber(int n)
+	{
+		int temp=n;
+		int sum=0;
+		int multi=1;
+		while(n>0)
+		{
+			int rem=n%10;
+			sum=sum+rem;
+			multi=multi*rem;
+			n=n/10;
+
+		}
+		if(sum==multi)
+		{
+			System.out.println(temp+" is a spy number" );
+		}
+		else
+		{
+			System.out.println(temp+" is not a spy number");
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number: ");
+		int n=sc.nextInt();
+		checkSpyNumber(n);
+	}
+
+}
