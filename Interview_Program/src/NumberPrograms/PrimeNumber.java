865a9216e6ae3e9956570a602cbfb91a89aaa568
AKASH KUMAR <akakash201@gmail.com>
2025-03-17 22:51:26 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/PrimeNumber.java
@@ -0,0 +1,29 @@
+package NumberPrograms;
+
+public class PrimeNumber
+{
+	public static void checkPrime(int n)
+	{
+		int count=0;
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			System.out.println(n+" is a prime number");
+		}
+		else
+		{
+			System.out.println(n+" is not a prime number");
+		}
+	}
+	public static void main(String[] args)
+	{
+		checkPrime(14);
+	}
+
+}
