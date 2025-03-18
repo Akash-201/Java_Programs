d06f728e460f1624980679ed818facad73fc09ce
AKASH KUMAR <akakash201@gmail.com>
2025-03-19 00:02:46 +0530

    .

---
+++ Interview_Program/src/NumberPrograms/SwapTwoNumber4.java
@@ -0,0 +1,18 @@
+package NumberPrograms;
+
+public class SwapTwoNumber4
+{
+	public static void swapTwoNumber(int a,int b)
+	{
+		System.out.println("Before Swap: a = " + a + ", b = " + b);
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		System.out.println("After Swap: a = " + a + ", b = " + b);
+	}
+	public static void main(String[] args)
+	{
+		swapTwoNumber(12,84);
+	}
+
+}
