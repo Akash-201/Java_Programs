cead45ca0fcd9035d03221b0f8a77c49f6f4d7ac
AKASH KUMAR <akakash201@gmail.com>
2025-03-20 22:16:07 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/ReverseInteger.java
@@ -0,0 +1,21 @@
+package CompanyQuestions;
+
+public class ReverseInteger
+{
+	public static void reverseInteger(int n)
+	{
+		int reverse=0;
+		while(n!=0)
+		{
+			int rem=n%10;
+			reverse=reverse*10+rem;
+			n=n/10;
+		}
+		System.out.println(reverse);
+	}
+	public static void main(String[] args)
+	{
+		reverseInteger(120);
+	}
+
+}
