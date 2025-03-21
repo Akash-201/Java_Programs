58ca6f1a60a4a602c58c0b7c997f8cf741290109
AKASH KUMAR <akakash201@gmail.com>
2025-03-21 21:58:25 +0530

    .

--- Interview_Program/src/Array/DistinctElements.java
+++ Interview_Program/src/Array/DistinctElements.java
@@ -8,12 +8,10 @@ public class DistinctElements
 	{
 		for(int i=0;i<arr.length;i++)
 		{
-			int count=1;
 			for(int j=i+1;j<arr.length;j++)
 			{
 				if(arr[i]==arr[j])
 				{
-					count++;
 					arr[j]=-1;
 				}
 			}
