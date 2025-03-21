e8b6ea610697bcec0286296ab543ee5940640a99
AKASH KUMAR <akakash201@gmail.com>
2025-03-21 21:59:04 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,33 @@
+package CompanyQuestions;
+
+public class RemoveDuplicateFromSortedArray
+{
+	public static void removeDuplicate(int[] arr)
+	{
+		int count=1;
+		for(int i=0;i<arr.length;i++) 
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					arr[j]=-1;
+					count++;
+				}
+			}
+			if(arr[i]!=-1)
+			{
+				System.out.print(arr[i]+" ");
+			}
+			
+		}
+		System.out.println("\n"+count);
+		
+	}
+	public static void main(String[] args)
+	{
+		int[] arr= {1,1,2};
+		removeDuplicate(arr);
+	}
+
+}
