71c57732a8a80e2f07334d8edc3703f8dd4bfac0
AKASH KUMAR <akakash201@gmail.com>
2025-03-21 22:15:51 +0530

    .

--- Interview_Program/src/CompanyQuestions/RemoveDuplicateFromSortedArray.java
+++ Interview_Program/src/CompanyQuestions/RemoveDuplicateFromSortedArray.java
@@ -4,7 +4,7 @@ public class RemoveDuplicateFromSortedArray
 {
 	public static void removeDuplicate(int[] arr)
 	{
-		int count=1;
+		int count=0;
 		for(int i=0;i<arr.length;i++) 
 		{
 			for(int j=i+1;j<arr.length;j++)
@@ -19,15 +19,16 @@ public class RemoveDuplicateFromSortedArray
 			{
 				System.out.print(arr[i]+" ");
 			}
-			
 		}
-		System.out.println("\nAfter removing duplicate ,size of array is: "+count);
+		System.out.println();
 		
 	}
 	public static void main(String[] args)
 	{
 		int[] arr= {1,1,2};
+		int[] arr1= {0,0,1,1,1,2,2,3,3,4};
 		removeDuplicate(arr);
+		removeDuplicate(arr1);
 	}
 
 }
