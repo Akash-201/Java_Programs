8bd0c3a90cfd502d4c47d0833645477a35e5f413
AKASH KUMAR <akakash201@gmail.com>
2025-03-27 22:04:32 +0530

    .

--- Interview_Program/src/String/LengthOfLastWord.java
+++ Interview_Program/src/String/LengthOfLastWord.java
@@ -5,7 +5,13 @@ public class LengthOfLastWord
 	public static int lengthOfLastWord(String s1)
 	{
 		String[] s2=s1.split(" ");	
-		return s2[s2.length-1].length();
+//		return s2[s2.length-1].length();
+		
+		 for(int i=s2.length-1;i>=0;i--)
+         {
+             return s2[i].length();
+         }
+         return s1.length();
 		
 		
 	}
