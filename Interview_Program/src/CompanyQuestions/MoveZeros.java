1f56c7913549914ebb48ab9d2428e45bb84e9e7d
AKASH KUMAR <akakash201@gmail.com>
2025-03-28 23:21:10 +0530

    .

--- Interview_Program/src/CompanyQuestions/MoveZeros.java
+++ Interview_Program/src/CompanyQuestions/MoveZeros.java
@@ -1,5 +1,9 @@
 package CompanyQuestions;
 
+//Given an integer array, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+//Note: that you must do this in-place without making a copy of the array.
+
 import java.util.Arrays;
 
 public class MoveZeros
