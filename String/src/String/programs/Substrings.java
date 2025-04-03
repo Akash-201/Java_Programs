3397b6361bd1572481eed5db8af387f5a1c0e8fc
AKASH KUMAR <akakash201@gmail.com>
2025-04-03 22:31:44 +0530

    .

---
+++ String/src/String/programs/Substrings.java
@@ -0,0 +1,23 @@
+package String.programs;
+
+public class Substrings
+{
+	public static void findAllSubstrings(String str)
+	{
+		System.out.println("All substrings of \"" + str + "\":");
+		
+		for(int i=0;i<str.length();i++)
+		{
+			for(int j=i+1;j<=str.length();j++)
+			{
+				System.out.println(str.substring(i, j));
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+        String str = "abc";
+        findAllSubstrings(str);
+	}
+
+}
