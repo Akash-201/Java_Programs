2b4461e4006ed6d8d74cd6dfa3d478b73f3b625e
AKASH KUMAR <akakash201@gmail.com>
2025-04-05 16:25:37 +0530

    .

---
+++ Imp_Programs/src/Important/programs/MoveZero.java
@@ -0,0 +1,29 @@
+package Important.programs;
+
+import java.util.Arrays;
+
+public class MoveZero 
+{
+	public static void moveZeros(int[] arr)
+	{
+		int index=0;
+		int[] newArr=new int[arr.length];
+		for(int i=0;i<arr.length;i++) 
+		{
+			if(arr[i]!=0)
+			{
+				newArr[index]=arr[i];
+				index++;
+			}
+			
+		}
+		System.out.println(Arrays.toString(newArr));
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr= {0,1,0,3,12,0,23};
+		moveZeros(arr);
+	}
+
+}
