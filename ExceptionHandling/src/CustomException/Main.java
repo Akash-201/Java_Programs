c0c18911d8ee4965e59777878f84fcd4e8133d8f
AKASH KUMAR <akakash201@gmail.com>
2025-04-07 16:59:22 +0530

    .

---
+++ ExceptionHandling/src/CustomException/Main.java
@@ -0,0 +1,36 @@
+package CustomException;
+
+class UnderAgeException extends Exception
+{
+	UnderAgeException()
+	{
+		// its call parent class constructor  means its provide description of default exception handler
+		super("You are under Age");
+	}
+	UnderAgeException(String message)
+	{
+		super(message);
+	}
+}
+public class Main
+{
+	public static void main(String[] args)
+	{
+		int age=18;
+		try 
+		{
+			if(age<18)
+			{
+				throw new UnderAgeException("You are not eligible for voting");
+			}
+			else
+			{
+				System.out.println("You are eligible for voting");
+			}
+		}
+		catch(UnderAgeException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
