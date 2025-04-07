4dbe869925f980dab8642aa93d0526d57999dd0c
AKASH KUMAR <akakash201@gmail.com>
2025-04-08 00:14:50 +0530

    .

---
+++ String/src/String/programs/FirstCapitalOtherSmall.java
@@ -0,0 +1,27 @@
+package String.programs;
+
+public class FirstCapitalOtherSmall
+{
+	public static void convert(String str)
+	{
+		String[] str2=str.split(" ");
+		String result="";
+
+		for(int i=0;i<str2.length;i++)
+		{
+			if(!str2[i].isEmpty())
+			{
+				String word=str2[i];
+				String capitalizedWord=Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase()+" ";
+				result=result+capitalizedWord;
+			}
+		}
+		System.out.println(result.trim());
+	}
+	public static void main(String[] args)
+	{
+		String str="jAVA iS eASY ";
+		convert(str);
+	}
+
+}
