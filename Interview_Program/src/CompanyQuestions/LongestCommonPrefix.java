34d99eb5428deff595dd3a1d32bae97c46906d42
AKASH KUMAR <akakash201@gmail.com>
2025-04-12 22:18:21 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package CompanyQuestions;
+// input str=["flower","flow","flight"]
+// output: "fl"
+
+// input str=["dog","racecar","car"]
+// output: ""
+
+
+public class LongestCommonPrefix 
+{
+	public static void longestCommonPrefix(String[] arr)
+	{
+		if(arr.length==0)
+		{
+			System.out.println("");
+			return;
+		}
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		String[] arr= {"dog","racecar","car"};
+		longestCommonPrefix(arr);
+		
+	}
+
+}
