475308bc8ec95c774ca9e703674b9905f059b84a
AKASH KUMAR <akakash201@gmail.com>
2025-04-18 19:09:03 +0530

    .

--- Interview_Program/src/CompanyQuestions/Zig_Zag.java
+++ Interview_Program/src/CompanyQuestions/Zig_Zag.java
@@ -12,16 +12,28 @@ public class Zig_Zag
 {
 	public static void printNumber(int[][] arr)
 	{
-		for(int i=0;i<arr.length;i++)
-		{
-			for(int j=0;j<1;j++)
-			{
-				System.out.print(arr[i][j]+" ");
-			}
+		for (int j = 0; j < arr[0].length; j++)
+        {
+            if (j % 2 == 0)
+            {
+               
+                for (int i = 0; i < arr.length; i++)
+                {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            else
+            {
+              
+                for (int i = arr.length - 1; i >= 0; i--)
+                {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+        }
+    }
 		
-		}
-		
-	}
+	
 	public static void main(String[] args)
 	{
 		int[][] arr= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
