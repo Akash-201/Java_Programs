7c23cdbd07be90b2eb70b5c2bda7085df7ef7d6f
AKASH KUMAR <akakash201@gmail.com>
2025-04-19 16:18:57 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/CustomException.java
@@ -0,0 +1,38 @@
+package CompanyQuestions;
+
+
+class UnderAgeException extends Exception
+{
+	 UnderAgeException() 
+	{
+		super("You are under Age");
+	}
+//	 UnderAgeException(String message)
+//	 {
+//		 super(message);
+//	 }
+}
+
+public class CustomException
+{
+	public static void main(String[] args)
+	{
+		int age=17;
+		try
+		{
+			if(age<18)
+			{
+				throw new UnderAgeException();
+			}
+			else
+			{
+				System.out.println("You are eligible for voting");
+			}
+		}
+		catch(UnderAgeException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
