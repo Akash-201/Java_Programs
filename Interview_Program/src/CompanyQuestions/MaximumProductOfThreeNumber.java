bd46a7764c18e672a24307091619116e8193f68c
AKASH KUMAR <akakash201@gmail.com>
2025-04-20 16:40:55 +0530

    .

--- Interview_Program/src/CompanyQuestions/MaximumProductOfThreeNumber.java
+++ Interview_Program/src/CompanyQuestions/MaximumProductOfThreeNumber.java
@@ -6,14 +6,14 @@ public class MaximumProductOfThreeNumber
 {
 	public static void maximumProduct(int[] arr)
 	{
-//		Arrays.sort(arr);
+		Arrays.sort(arr);
 		int product;
 		product=arr[arr.length-1]*arr[arr.length-2]*arr[arr.length-3];
 		System.out.println("Product is: "+product);
 	}
 	public static void main(String[] args)
 	{
-		int[] arr= {-1,-2,-3};
+		int[] arr= {-4,3,-1,-2,-8};
 		maximumProduct(arr);
 	}
 
