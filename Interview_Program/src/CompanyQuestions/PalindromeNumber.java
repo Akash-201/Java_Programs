27e1cf49f6025888ca82a451899eff3976bfa456
AKASH KUMAR <akakash201@gmail.com>
2025-04-21 23:16:12 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package CompanyQuestions;
+
+public class PalindromeNumber
+{
+	public static boolean checkPalindrome(int n)
+	{
+		int temp=n;
+		int rev=0;
+		while(n>0)
+		{
+			int rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+		if(temp==rev)
+		{
+			return true;
+		}
+		return false;
+		
+		
+	}
+	public static void main(String[] args) {
+		int n=121;
+		System.out.println(checkPalindrome(n));
+	}
+
+}
