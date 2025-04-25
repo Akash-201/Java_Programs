a8322108def5c10e728c96a3d3949999830eb2a0
AKASH KUMAR <akakash201@gmail.com>
2025-04-25 16:01:39 +0530

    .

---
+++ Interview_Program/src/InterviewerAsked/ReverseSentence.java
@@ -0,0 +1,21 @@
+package InterviewerAsked;
+
+public class ReverseSentence 
+{
+    public static void reverseSentence(String s1)
+    {
+        String[] s2 = s1.split(" ");
+        String reverse = "";
+        for (int i = s2.length - 1; i >= 0; i--) 
+        {
+            reverse += s2[i] + " ";
+        }
+        System.out.println(reverse.trim());
+    }
+
+    public static void main(String[] args) 
+    {
+        String s1 = "This is java";
+        reverseSentence(s1);
+    }
+}
