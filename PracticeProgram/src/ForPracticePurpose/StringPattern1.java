0ddc2850ce01b3df978978be894eb4b60c820d4e
AKASH KUMAR <akakash201@gmail.com>
2025-04-27 17:25:01 +0530

    .

---
+++ PracticeProgram/src/ForPracticePurpose/StringPattern1.java
@@ -0,0 +1,37 @@
+package ForPracticePurpose;
+// input: a2b3c4
+// output: aabbbcccc
+
+public class StringPattern1
+{
+	public static void pattern(String str)
+	{
+		char[] ch=str.toCharArray();
+		for(int i=0;i<ch.length;i++) 
+		{
+			if((ch[i]>=97 && ch[i]<=122) || (ch[i]>=65 && ch[i]<=90))
+		
+			{
+				System.out.print(ch[i]);
+				
+			}
+			else
+			{
+				int count = ch[i]-'0';
+				for(int j=1;j<count;j++)
+				{
+					System.out.print(ch[i-1]);
+				}
+			}
+			
+			
+		}
+		
+	}
+	public static void main(String[] args)
+	{
+		String str="a2b3c4D5";
+		pattern(str);
+	}
+
+}
