6e266bc55051eb669448a8cf73219f18301e6712
AKASH KUMAR <akakash201@gmail.com>
2025-05-01 22:18:33 +0530

    .

--- Interview_Program/src/InterviewerAsked/CharacterCountInSentence.java
+++ Interview_Program/src/InterviewerAsked/CharacterCountInSentence.java
@@ -1,5 +1,9 @@
 package InterviewerAsked;
 
+//Task: Need to avoid single instance characters, spaces and it is case sensitive
+//
+//You need to print - character name - count
+
 public class CharacterCountInSentence 
 {
 	public static void countCharacter(String str)
