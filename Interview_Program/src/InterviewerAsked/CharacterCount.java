d2c655347498c8405d744c223d0e38784d11cf7a
AKASH KUMAR <akakash201@gmail.com>
2025-05-02 16:19:52 +0530

    .

---
+++ Interview_Program/src/InterviewerAsked/CharacterCount.java
@@ -0,0 +1,30 @@
+package InterviewerAsked;
+
+public class CharacterCount 
+{
+	public static void countCharacter(String str)
+	{
+		char[] ch=str.toCharArray();
+		for(int i=0;i<ch.length;i++)
+		{
+			if(ch[i]==' ')
+				continue;
+			int count=1;
+			for(int j=i+1;j<ch.length;j++)
+			{
+				if(ch[i]==ch[j])
+				{
+					count++;
+					ch[j]=' ';
+				}
+			}
+			System.out.println(ch[i]+" --> "+count);
+		}
+	}
+	public static void main(String[] args)
+	{
+		String str="Akash is a software engineer";
+		countCharacter(str);
+	}
+
+}
