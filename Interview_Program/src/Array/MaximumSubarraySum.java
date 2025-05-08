22f8680e6e0a7c7aed5aaaabd98d2d7e503ec685
AKASH KUMAR <akakash201@gmail.com>
2025-05-08 17:09:30 +0530

    .

--- Interview_Program/src/Array/MaximumSubarraySum.java
+++ Interview_Program/src/Array/MaximumSubarraySum.java
@@ -1,7 +1,19 @@
 package Array;
+//Given an array arr[], the task is to find the subarray that has the maximum sum and return its sum.
+
+//Examples:
+
 //Input: arr[] = {2, 3, -8, 7, -1, 2, 3}
 //Output: 11
 //Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+
+//Input: arr[] = {-2, -4}
+//Output: –2
+//Explanation: The subarray {-2} has the largest sum -2.
+
+//Input: arr[] = {5, 4, 1, 7, 8}
+//Output: 25
+//Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
 
 public class MaximumSubarraySum 
 {
@@ -27,7 +39,7 @@ public class MaximumSubarraySum
 	}
 	public static void main(String[] args)
 	{
-		int[] arr= {5, 4, 1, 7, 8};
+		int[] arr= {2, 3, -8, 7, -1, 2, 3};
 		maximumSubarraySum(arr);
 	}
 
