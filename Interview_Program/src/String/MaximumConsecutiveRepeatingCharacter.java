90cc47c9d5e0880c1aa1baa15a1c58c1f506241e
AKASH KUMAR <akakash201@gmail.com>
2025-05-09 17:47:14 +0530

    .

--- Interview_Program/src/String/MaximumConsecutiveRepeatingCharacter.java
+++ Interview_Program/src/String/MaximumConsecutiveRepeatingCharacter.java
@@ -1,5 +1,19 @@
 package String;
 
+//Given a string s, the task is to find the maximum consecutive repeating character in the string.
+
+//Note: We do not need to consider the overall count, but the count of repeating that appears in one place.
+
+//Examples: 
+
+//Input: s = “geeekk”
+//Output: e
+//Explanation: character e comes 3 times consecutively which is maximum.
+
+//Input: s = “aaaabbcbbb”
+//Output: a
+//Explanation: character a comes 4 times consecutively which is maximum.
+
 
 public class MaximumConsecutiveRepeatingCharacter 
 {
