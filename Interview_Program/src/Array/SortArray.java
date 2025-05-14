534d77a0eb9698e160f0f747c8af1113bd895ff4
AKASH KUMAR <akakash201@gmail.com>
2025-05-14 17:19:38 +0530

    .

---
+++ Interview_Program/src/Array/SortArray.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SortArray 
+{
+	public static void sortArray(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+	public static void main(String[] args)
+	{
+		int[] arr= {4,6,2,9,33,24,1,7,0,-8,6};
+		sortArray(arr);
+	}
+
+}
