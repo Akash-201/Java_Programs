bed53201065c0491f21e4f4334ca582e79534500
AKASH KUMAR <akakash201@gmail.com>
2025-05-15 17:51:58 +0530

    .

--- Interview_Program/src/String/MedianOfTwoSortedArrays.java
+++ Interview_Program/src/String/MedianOfTwoSortedArrays.java
@@ -1,4 +1,18 @@
 package String;
+//
+//Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+//
+//Example 1:
+
+//Input: nums1 = [1,3], nums2 = [2]
+//Output: 2.00000
+//Explanation: merged array = [1,2,3] and median is 2.
+
+//Example 2:
+
+//Input: nums1 = [1,2], nums2 = [3,4]
+//Output: 2.50000
+//Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 import java.util.Arrays;
 
@@ -19,13 +33,26 @@ public class MedianOfTwoSortedArrays
 		Arrays.sort(mergedArr);
 		System.out.println("Merged and Sorted Array: " + Arrays.toString(mergedArr));
 		
+		double median;
+		int n=mergedArr.length;
+		if(n%2==0)
+		{
+			median=(mergedArr[n/2-1]+mergedArr[n/2])/2.0;
+		}
+		else
+		{
+			median=mergedArr[n/2];
+		}
+		
+		System.out.println("Median is: " + median);
+		
 		
 		
 	}
 	public static void main(String[] args)
 	{
-		int[] arr1= {1,3};
-		int[] arr2= {2};
+		int[] arr1= {1,2};
+		int[] arr2= {3,4};
 		median(arr1, arr2);		
 	}
 
