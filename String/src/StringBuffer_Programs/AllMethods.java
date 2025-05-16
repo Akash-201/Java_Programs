eded9d5216f1a9326b0a92d97f30d6e29ed0276c
AKASH KUMAR <akakash201@gmail.com>
2025-05-16 17:11:18 +0530

    .

--- String/src/StringBuffer_Programs/AllMethods.java
+++ String/src/StringBuffer_Programs/AllMethods.java
@@ -6,6 +6,21 @@ public class AllMethods
 	{
 		StringBuffer str=new StringBuffer("WELCOME");
 		System.out.println(str);
+		str.append(" JAVA");
+		System.out.println(str);
+		System.out.println(str.insert(7," TO"));
+		System.out.println(str.length());
+		System.out.println(str.delete(7,10));
+		str.reverse();
+		System.out.println(str);
+		System.out.println(str.reverse());
+		System.out.println(str.replace(3, 6, "Hi"));
+		System.out.println(str.replace(3, 6, "COME"));
+		System.out.println(str.substring(3));
+		System.out.println(str.indexOf("COME"));
+		System.out.println(str.charAt(5));
+		System.out.println(str.subSequence(2,5));
+		System.out.println(str);
 	}
 
 }
