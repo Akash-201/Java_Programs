f8c73eca1466698e8e77120020066b0ec9deb759
AKASH KUMAR <akakash201@gmail.com>
2025-05-16 16:59:13 +0530

    .

---
+++ String/src/StringBuffer_Programs/AllMethods.java
@@ -0,0 +1,11 @@
+package StringBuffer_Programs;
+
+public class AllMethods
+{
+	public static void main(String[] args)
+	{
+		StringBuffer str=new StringBuffer("WELCOME");
+		System.out.println(str);
+	}
+
+}
