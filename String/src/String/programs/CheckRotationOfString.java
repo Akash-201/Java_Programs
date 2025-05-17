0effd086c2908e8c90fd5ed5dc7f1cf52c24851a
AKASH KUMAR <akakash201@gmail.com>
2025-05-17 16:37:46 +0530

    .

---
+++ String/src/String/programs/CheckRotationOfString.java
@@ -0,0 +1,24 @@
+package String.programs;
+
+
+public class CheckRotationOfString 
+{
+	public static boolean checkRotation(String str1,String str2)
+	{
+		if(str1.length()!=str2.length())
+		{
+			return false;
+		}
+		String combined=str1+str1;
+
+		return combined.contains(str2);
+	}
+	public static void main(String[] args)
+	{
+		String str1 = "waterbottle";
+		String str2 = "erbottlewat";
+
+		System.out.println("Is Rotation: " + checkRotation(str1, str2));
+	}
+
+}
