3e3c8c84dfffaf3251de4789c27791feb613c935
AKASH KUMAR <akakash201@gmail.com>
2025-05-18 16:28:29 +0530

    .

---
+++ String/src/String/programs/CheckSubsequence.java
@@ -0,0 +1,42 @@
+package String.programs;
+
+
+public class CheckSubsequence 
+{
+	public static void checkSubsequence(String str1,String str2)
+	{
+		int count=0;
+		for(int i=0;i<str1.length();i++)
+		{
+			char ch1=str1.charAt(i);
+			
+			for(int j=0;j<str2.length();j++)
+			{
+				char ch2=str2.charAt(j);
+				if(ch1==ch2)
+				{
+					count++;
+					str2=str2.substring(j+1);
+					break;
+				}
+			}
+		}
+		if(count==str1.length())
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+		
+	}
+	public static void main(String[] args)
+	{
+		String str1="abc";
+		String str2="ahbgdc";
+		checkSubsequence(str1, str2);
+		
+	}
+
+}
