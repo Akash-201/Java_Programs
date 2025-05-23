0316de2f388c105734294be2c50f018ff99cbe18
AKASH KUMAR <akakash201@gmail.com>
2025-05-23 18:12:48 +0530

    .

---
+++ PracticeProgram/src/ForPracticePurpose/CommonElementInTwoArray.java
@@ -0,0 +1,30 @@
+package ForPracticePurpose;
+
+public class CommonElementInTwoArray
+{
+	public static void commonElement(int[] arr1,int[] arr2)
+	{
+		int[] newArr;
+		for(int i=0;i<arr1.length;i++)
+		{
+			for(int j=0;j<arr2.length;j++)
+			{
+				if(arr1[i]==arr2[j])
+				{
+					System.out.println(arr1[i]);
+					
+				}
+
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		int[] arr1= {2,1,4,6,3};
+		int[] arr2= {1,7,8,3,2};
+		commonElement(arr1, arr2);
+		
+		
+	}
+
+}
