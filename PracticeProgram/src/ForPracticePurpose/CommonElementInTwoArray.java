a53285e74f27c1cc68120864a17b285a84d0a7af
AKASH KUMAR <akakash201@gmail.com>
2025-05-24 11:18:56 +0530

    .

--- PracticeProgram/src/ForPracticePurpose/CommonElementInTwoArray.java
+++ PracticeProgram/src/ForPracticePurpose/CommonElementInTwoArray.java
@@ -1,29 +1,36 @@
 package ForPracticePurpose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommonElementInTwoArray
 {
 	public static void commonElement(int[] arr1,int[] arr2)
 	{
+		ArrayList<Integer> commonElement=new ArrayList<>();
 		for(int i=0;i<arr1.length;i++)
 		{
 			for(int j=0;j<arr2.length;j++)
 			{
 				if(arr1[i]==arr2[j])
 				{
-					System.out.println(arr1[i]);
-					
+					if(!commonElement.contains(arr1[i]))
+					{
+						commonElement.add(arr1[i]);
+					}
+					break;
 				}
-				
 			}
 		}
+		System.out.println(commonElement);
 	}
 	public static void main(String[] args)
 	{
 		int[] arr1= {2,1,4,6,3};
 		int[] arr2= {1,7,8,3,2};
 		commonElement(arr1, arr2);
-		
-		
+
+
 	}
 
 }
