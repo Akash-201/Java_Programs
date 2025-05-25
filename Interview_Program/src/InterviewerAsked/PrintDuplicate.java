f36b7f7fea233906f19fda510362c11de48b5c3e
AKASH KUMAR <akakash201@gmail.com>
2025-05-25 16:27:15 +0530

    .

---
+++ Interview_Program/src/InterviewerAsked/PrintDuplicate.java
@@ -0,0 +1,29 @@
+package InterviewerAsked;
+
+import java.util.HashSet;
+
+public class PrintDuplicate 
+{
+	public static void printDuplicate(int[] arr)
+	{
+		HashSet<Integer> duplicate=new HashSet<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j] && !duplicate.contains(arr[i]))
+				{
+					System.out.print(arr[i]+" ");
+					duplicate.add(arr[i]);
+//					break;
+				}
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		int[] arr= {1,4,2,7,1,7,1,9,1};
+		printDuplicate(arr);
+	}
+
+}
