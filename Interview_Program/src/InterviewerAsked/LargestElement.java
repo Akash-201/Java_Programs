34b37e2018e76b97a7f79ffbd42cbd07130f8cee
AKASH KUMAR <akakash201@gmail.com>
2025-05-31 16:24:16 +0530

    .

--- Interview_Program/src/InterviewerAsked/LargestElement.java
+++ Interview_Program/src/InterviewerAsked/LargestElement.java
@@ -6,14 +6,24 @@ public class LargestElement
 {
 	public static void findLargestElement(int[] arr)
 	{
-		Arrays.sort(arr);
-		{
-			System.out.println(arr[arr.length-1]);
-		}
+		
+		// using sorting method
+//		Arrays.sort(arr);
+//		{
+//			System.out.println(arr[arr.length-1]);
+//		}
+		
+		
+		// if Array already sorted
+		System.out.println(arr[arr.length-1]);
+		
+		
+		
+		
 	}
 	public static void main(String[] args)
 	{
-		int[] arr= {6,3,61,38,4,87};
+		int[] arr= {6,7,98,123};
 		findLargestElement(arr);
 	}
 
