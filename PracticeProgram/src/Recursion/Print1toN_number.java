f37f94c11dcc5a4517868f17d5f27503f4c33f31
AKASH KUMAR <akakash201@gmail.com>
2025-06-01 19:21:42 +0530

    .

---
+++ PracticeProgram/src/Recursion/Print1toN_number.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Print1toN_number
+{
+	public static void printNumber(int n)
+	{
+		if(n==1)
+		{
+			System.out.println(n);
+			return;
+		}
+		
+		printNumber(n-1);
+		System.out.println(n);
+		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number: ");
+		int n=sc.nextInt();
+		printNumber(n);
+	}
+
+}
