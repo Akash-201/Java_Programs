63cfb3c0415e41e848f857feaaa0a4111662ed18
AKASH KUMAR <akakash201@gmail.com>
2025-06-01 19:25:42 +0530

    .

--- PracticeProgram/src/Recursion/Print1toN_number.java
+++ PracticeProgram/src/Recursion/Print1toN_number.java
@@ -11,19 +11,22 @@ public class Print1toN_number
 			System.out.println(n);
 			return;
 		}
-		
+
 		printNumber(n-1);
 		System.out.println(n);
-		
+
 	}
-	
-	
+
+
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter any number: ");
-		int n=sc.nextInt();
-		printNumber(n);
+		while(true)
+		{
+			System.out.print("Enter any number: ");
+			int n=sc.nextInt();
+			printNumber(n);
+		}
 	}
 
 }
