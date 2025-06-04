9530b07bc0addeef317fe763dfcca78e9719fbc7
AKASH KUMAR <akakash201@gmail.com>
2025-06-04 18:10:58 +0530

    .

--- PracticeProgram/src/Recursion/NthFibonacciNumber.java
+++ PracticeProgram/src/Recursion/NthFibonacciNumber.java
@@ -1,17 +1,15 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class NthFibonacciNumber 
 {
 	public static int fibonacciNumber(int n)
 	{
 		// base case
-		if(n==0 )
+		if(n==0 || n==1)
 		{
-			return 0;
-		}
-		if(n==1)
-		{
-			return 1;
+			return n;
 		}
 
 		// recursive work
@@ -24,8 +22,13 @@ public class NthFibonacciNumber
 	}
 	public static void main(String[] args)
 	{
-		int n=7;
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+		System.out.print("Enter any number: ");
+		int n=sc.nextInt();
 		System.out.println(fibonacciNumber(n));
+		}
 	}
 
 }
