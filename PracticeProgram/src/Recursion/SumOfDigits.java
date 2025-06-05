3dd89375d3d87ff5e24b0f4a1895341bf94cf11f
AKASH KUMAR <akakash201@gmail.com>
2025-06-05 16:49:03 +0530

    .

--- PracticeProgram/src/Recursion/SumOfDigits.java
+++ PracticeProgram/src/Recursion/SumOfDigits.java
@@ -4,19 +4,26 @@ public class SumOfDigits
 {
 	public static int sumOfDigits(int n)
 	{
+		// Base Case
 		if(n>=0 && n<=9)
 		{
 			return n;
 		}
-		int lastDigit=n%10;
-		int sum=0;
-		sum=sum+sumOfDigits(n/10);
 		
-		return sum+lastDigit;
+		// 1st way
+		
+//		int lastDigit=n%10;
+//		int sum=0;
+//		sum=sum+sumOfDigits(n/10);
+//		return sum+lastDigit;
+		
+		
+		// 2nd way
+		return (n%10)+sumOfDigits(n/10);
 	}
 	public static void main(String[] args)
 	{
-		int n=1;
+		int n=12345;
 		System.out.println(sumOfDigits(n));
 		
 	}
