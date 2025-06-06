28abaa420126ce9e69ab7356212946f00ffbd76b
AKASH KUMAR <akakash201@gmail.com>
2025-06-06 17:51:05 +0530

    .

---
+++ PracticeProgram/src/Recursion/SumOfN_Natural_Number.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class SumOfN_Natural_Number 
+{
+	public static int sumOfNumber(int n)
+	{
+		// base case
+		
+		if(n==1)
+		{
+			return n;
+		}
+		
+		// recursive work
+		int sum=sumOfNumber(n-1);
+		
+		// self work
+		return n+sum;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(sumOfNumber(10));
+	}
+
+}
