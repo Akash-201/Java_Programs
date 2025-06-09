4d823f09052932df5f66215694cd468114c1596e
AKASH KUMAR <akakash201@gmail.com>
2025-06-09 17:25:29 +0530

    .

--- Imp_Programs/src/Important/programs/GCD3.java
+++ Imp_Programs/src/Important/programs/GCD3.java
@@ -1,5 +1,7 @@
 package Important.programs;
 
+import java.util.Scanner;
+
 //Efficient Iterative GCD (Euclidean Algorithm)
 
 public class GCD3 
@@ -17,7 +19,13 @@ public class GCD3
 	}
 	public static void main(String[] args)
 	{
-		System.out.println(findGCD(12,18));
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter First number: ");
+		int x=sc.nextInt();
+		System.out.print("Enter second number: ");
+		int y=sc.nextInt();
+		
+		System.out.println(findGCD(x,y));
 	}
 
 }
