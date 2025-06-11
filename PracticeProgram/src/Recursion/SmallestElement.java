9502e0a6786d55ee12f8a092e314950ba1803a09
AKASH KUMAR <akakash201@gmail.com>
2025-06-11 16:45:43 +0530

    .

---
+++ PracticeProgram/src/Recursion/SmallestElement.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class SmallestElement
+{
+	public static int smallElement(int[] arr,int idx)
+	{
+		// base case
+		if(idx==arr.length-1)
+			return arr[idx];
+		
+		// recursive work
+		int ans= smallElement(arr, idx+1);
+		
+		return Math.min(arr[idx], ans);
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr= {5,34,2,65,356};
+		
+		System.out.println(smallElement(arr, 0));
+	}
+
+}
