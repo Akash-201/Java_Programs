ec8b86de27024ebf7be3da1babd0e41f594f9af1
AKASH KUMAR <akakash201@gmail.com>
2025-06-18 17:05:38 +0530

    .

--- Demo/src/Basic/BreakContinue.java
+++ Demo/src/Basic/BreakContinue.java
@@ -6,8 +6,14 @@ public class BreakContinue
 		
 		for(int i=10;i<=50;i++)
 		{
+			
+			if(i%5==0)
+			{
+				continue;
+			}
 			System.out.print(i+" ");
-			if(i==20)
+			
+			if(i==44)
 			{
 				break;
 			}
