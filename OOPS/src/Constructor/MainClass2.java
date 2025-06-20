6ad8164aa0c216bc407ac65cace9eec5029b0c06
AKASH KUMAR <akakash201@gmail.com>
2025-06-20 16:48:24 +0530

    .

--- OOPS/src/Constructor/MainClass2.java
+++ OOPS/src/Constructor/MainClass2.java
@@ -2,6 +2,18 @@ package Constructor;
 
 public class MainClass2
 {
+	public static void main(String[] args)
+	{
+		Student_ConstructorOverloading sco1=new Student_ConstructorOverloading(101);
+		System.out.println("Student id is "+sco1.studentId);
+		
+		Student_ConstructorOverloading sco2=new Student_ConstructorOverloading(102,"Rahul","Patna");
+		System.out.println("Student id is "+sco2.studentId);
+		System.out.println("Student name is "+sco2.studentName);
+		System.out.println("Student city name is "+sco2.studentCity);
+
+
+	}
 	
 
 }
