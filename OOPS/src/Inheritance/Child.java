d9cac97b4fd8f14bc215c14ac9bf043d8f0826f3
AKASH KUMAR <akakash201@gmail.com>
2025-06-21 18:04:21 +0530

    .

---
+++ OOPS/src/Inheritance/Child.java
@@ -0,0 +1,47 @@
+package Inheritance;
+
+class GrandFather
+{
+	String land="10 Acre";
+	String home="2 Home";
+	
+	public void hobby()
+	{
+		System.out.println("Travelling");
+	}
+}
+
+class Father extends GrandFather
+{
+	String land="15 Acre";
+	String home="3 Home";
+	int car=2;
+	
+	@Override
+	public void hobby()
+	{
+		System.out.println("Travelling and Singing");
+	}
+	
+}
+
+public class Child extends Father
+{
+	int Bike=1;
+	
+	@Override
+	public void hobby()
+	{
+		System.out.println("Travelling, Dancing, Reading...");
+	}
+	
+	public static void main(String[] args)
+	{
+		Child c=new Child();
+		c.hobby();
+		
+	}
+	
+	
+
+}
