374b42033be952a220524d30d0de90ef79f43c9e
AKASH KUMAR <akakash201@gmail.com>
2025-06-22 16:30:49 +0530

    .

---
+++ OOPS/src/Inheritance/TestHierarchical.java
@@ -0,0 +1,43 @@
+package Inheritance;
+
+//Hierarchical Inheritance
+
+class Animal1
+{
+	public void eat()
+	{
+		System.out.println("Animal is eating");
+	}
+}
+
+class Dog1 extends Animal1
+{
+	void bark()
+	{
+		System.out.println("Dog is barking");
+	}
+}
+
+class  Cat1 extends Animal1
+{
+	void meow()
+	{
+		System.out.println("Cat is meowing");
+	}
+}
+
+public class TestHierarchical 
+{
+	public static void main(String[] args)
+	{
+		Dog1 d=new Dog1();
+		d.eat();
+		d.bark();
+
+		Cat1 c=new Cat1();
+		c.eat();
+		c.meow();
+	}
+
+
+}
