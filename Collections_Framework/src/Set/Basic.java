c8d30820b709d15802a26c03ae8f5d5aba030811
AKASH KUMAR <akakash201@gmail.com>
2025-06-26 19:01:45 +0530

    .

---
+++ Collections_Framework/src/Set/Basic.java
@@ -0,0 +1,21 @@
+package Set;
+
+import java.util.HashSet;
+
+public class Basic
+{
+	public static void main(String[] args) {
+		System.out.println("Start Coding...");
+		
+		HashSet<Double> set= new HashSet<>();
+		set.add(24.45);
+		set.add(456.23);
+		set.add(34.00);
+		set.add(45.45);
+		
+		
+		System.out.println(set);
+		
+	}
+
+}
