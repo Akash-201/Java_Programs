5aad7a243d17ffae71fd46202f0e28d3890b5835
AKASH KUMAR <akakash201@gmail.com>
2025-06-26 19:28:41 +0530

    .

--- Collections_Framework/src/Map/Basic.java
+++ Collections_Framework/src/Map/Basic.java
@@ -5,15 +5,17 @@ import java.util.HashMap;
 public class Basic 
 {
 	public static void main(String[] args) {
-		HashMap<String, Integer> course = new HashMap<>();
+		HashMap<String, Integer> courses = new HashMap<>();
 		
-		course.put("Core Java",5000);
-		course.put("Advance Java",2000);
-		course.put("Framework",3000);
-		course.put("SQL",1500);
-		course.put("Web",3000);
+		courses.put("Core Java",5000);
+		courses.put("Advance Java",2000);
+		courses.put("Framework",3000);
+		courses.put("SQL",1500);
+		courses.put("Web",3000);
 		
-		System.out.println(course);
+		System.out.println(courses);
+		
+		System.out.println(courses.get("Core Java"));
 		
 	}
 
