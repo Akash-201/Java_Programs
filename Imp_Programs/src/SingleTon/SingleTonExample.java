a5241ef063b3b51de520a697d0f951d3a37d5efb
AKASH KUMAR <akakash201@gmail.com>
2025-06-29 18:29:27 +0530

    .

---
+++ Imp_Programs/src/SingleTon/SingleTonExample.java
@@ -0,0 +1,35 @@
+package SingleTon;
+class Samosa
+{
+	private static Samosa samosa;
+	
+	private Samosa()
+	{
+		
+	}
+	
+	public static Samosa getSamosa()
+	{
+		if(samosa==null)
+		{
+			samosa=new Samosa();
+		}
+		return samosa;
+	}
+}
+
+public class SingleTonExample
+{
+	public static void main(String[] args) 
+	{
+		Samosa samosa1=Samosa.getSamosa();
+		System.out.println(samosa1.hashCode());
+		
+		Samosa samosa2=Samosa.getSamosa();
+		System.out.println(samosa2.hashCode());
+		
+		
+		
+	}
+
+}
