39c9b7fe86fea3159c1954ee80ff6a171b9ce581
AKASH KUMAR <akakash201@gmail.com>
2025-06-30 18:52:03 +0530

    .

--- Pattern_Programs/src/Numbers_Pattern/Floyds_Triangle.java
+++ Pattern_Programs/src/Numbers_Pattern/Floyds_Triangle.java
@@ -1,13 +1,14 @@
 package Numbers_Pattern;
 
-//WAJP to print Floyd's triangle.
-//•   Input: n = 5
-//•   Output: 
-//•   1 
-//•   2 3 
-//•   4 5 6 
-//•   7 8 9 10 
-//•   11 12 13 14 15 
+/* WAJP to print Floyd's triangle.
+  Input: n = 5
+   Output: 
+   1 
+   2 3 
+   4 5 6 
+   7 8 9 10 
+   11 12 13 14 15 
+  */
 
 import java.util.Scanner;
 
