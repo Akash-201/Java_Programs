526be9270c59ed8dce50de1d27181349c2720e8c
AKASH KUMAR <akakash201@gmail.com>
2025-07-01 19:19:23 +0530

    .

--- Interview_Program/src/_4CECloudLabs/FindDuplicate.java
+++ Interview_Program/src/_4CECloudLabs/FindDuplicate.java
@@ -4,25 +4,31 @@ public class FindDuplicate
 {
 	public static void printDuplicate(int[] arr)
 	{
+		
 		for(int i=0;i<arr.length;i++)
 		{
-			if(arr[i]== -1)
+			int count=1;
+			if(arr[i]==-1)
 				continue;
 			for(int j=i+1;j<arr.length;j++)
 			{
 				if(arr[i]==arr[j])
 				{
-					System.out.print(arr[i]+" ");
+					count++;
 					arr[j]=-1;
 				}
 				
+			}
+			if(count>1 )
+			{
+				System.out.print(arr[i]+" ");
 			}
 		}
 	}
 	
 	public static void main(String[] args)
 	{
-		int[] arr= {2,5,3,6,2,3,9};
+		int[] arr= {2,5,3,6,2,3,9,2};
 		printDuplicate(arr);
 	}
 
