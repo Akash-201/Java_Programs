384328b2bfd4e2e51f738ebc20a1a51fc046f215
AKASH KUMAR <akakash201@gmail.com>
2025-07-03 18:44:28 +0530

    .

---
+++ Interview_Program/src/_4CECloudLabs/RemoveExtraSpace.java
@@ -0,0 +1,13 @@
+package _4CECloudLabs;
+
+public class RemoveExtraSpace
+{
+	public static void main(String[] args)
+	{
+		String before="  this     is    java";
+		String after=before.trim();
+		System.out.println(after);
+		
+	}
+
+}
