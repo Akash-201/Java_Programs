082062201fbdf6d930b4bc6c2948bdb1d4da7d2c
AKASH KUMAR <akakash201@gmail.com>
2025-07-03 18:49:52 +0530

    .

--- Interview_Program/src/_4CECloudLabs/RemoveExtraSpace.java
+++ Interview_Program/src/_4CECloudLabs/RemoveExtraSpace.java
@@ -5,7 +5,10 @@ public class RemoveExtraSpace
 	public static void main(String[] args)
 	{
 		String before="  This     Is    Java";
+		
+		 //replace multiple spaces with a single space
 		String after=before.trim().replaceAll("\\s+"," ");
+		
 		System.out.println(after);
 		
 	}
