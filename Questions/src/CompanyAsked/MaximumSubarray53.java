c20b3e261c5718191d7a98dd2251beaceba4d1f2
AKASH KUMAR <akakash201@gmail.com>
2025-07-11 19:17:51 +0530

    .

--- Questions/src/CompanyAsked/MaximumSubarray53.java
+++ Questions/src/CompanyAsked/MaximumSubarray53.java
@@ -17,7 +17,11 @@ public class MaximumSubarray53
 	public static void main(String[] args)
 	{
 		int[] nums= {-2,1,-3,4,-1,2,1,-5,4};
+		int[] nums2= {1};
+		int[] nums3= {5,4,-1,7,8};
 		System.out.println(maxSubArray(nums));
+		System.out.println(maxSubArray(nums2));
+		System.out.println(maxSubArray(nums3));
 		
 	}
 
