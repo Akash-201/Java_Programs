2a51ab8b4e841393b330de663bd8989b0ca0f3d3
AKASH KUMAR <akakash201@gmail.com>
2025-07-14 22:18:40 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/KthDistinctElement.java
@@ -0,0 +1,42 @@
+package CompanyQuestions;
+
+public class KthDistinctElement
+{
+	public static int distinctElement(int[] arr, int k)
+	{
+		int count1=0;
+		for(int i=0;i<arr.length;i++) 
+		{
+			if(arr[i]==-1)
+				continue;
+			int count=0;
+			
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					count++;
+					arr[j]=-1;
+				}
+			}
+			if(count==0)
+			{
+				count1++;
+				if(count1==k)
+				{
+					return arr[i];
+					
+				}
+			}
+		}
+		return -1;
+		
+	}
+	public static void main(String[] args)
+	{
+		int[] arr= {1,3,2,4,3};
+		int k=3;
+		System.out.println(distinctElement(arr, k));
+		
+	}
+}
