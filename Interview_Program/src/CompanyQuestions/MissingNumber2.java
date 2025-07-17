f060f8eb407255ea320407e1ed4a4d7730fb0ed4
AKASH KUMAR <akakash201@gmail.com>
2025-07-17 19:23:43 +0530

    .

--- Interview_Program/src/CompanyQuestions/MissingNumber2.java
+++ Interview_Program/src/CompanyQuestions/MissingNumber2.java
@@ -1,14 +1,24 @@
 package CompanyQuestions;
 
+/* Given an array of size n containing numbers from 1 to (n+1) with exactly one number missing,
+find the missing number.
+
+Example:
+
+Input:  [1, 2, 3, 4, 5, 7]
+Output: 6
+
+ */
+
 public class MissingNumber2
 {
 	public static int findMissingNumber(int[] arr)
 	{
 		int n=arr.length;
 		int totalSum=(n+1)*(n+2)/2;
-		
+
 		int currentSum=0;
-		
+
 		for(int val : arr)
 		{
 			currentSum+=val;
