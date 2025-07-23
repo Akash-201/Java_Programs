f21715ffca2d9bfa5ca183af93fab73f793df69c
AKASH KUMAR <akakash201@gmail.com>
2025-07-23 21:33:03 +0530

    .

---
+++ Questions/src/CompanyAsked/LemonadeChange860.java
@@ -0,0 +1,45 @@
+package CompanyAsked;
+
+public class LemonadeChange860
+{
+	public static boolean lemonadeChange(int[] bills)
+	{
+		int five=0;
+		int ten=0;
+		for(int bill : bills)
+		{
+			if(bill==5)
+			{
+				five++;
+			}
+			else if(bill==10)
+			{
+				if(five==0)
+					return false;
+				five--;
+				ten++;
+			}
+			else
+			{
+				if(five>0 && ten>0)
+				{
+					five--;
+					ten--;
+				}
+				else if(five>=3)
+				{
+					five=five-3;
+				}
+				else return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) 
+	{
+		int[] bills= {5,5,10,10,20};
+		System.out.println(lemonadeChange(bills));
+
+	}
+
+}
