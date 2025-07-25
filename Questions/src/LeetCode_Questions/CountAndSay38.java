b31a17b6bf827f9103e5051c48785102eae4d438
AKASH KUMAR <akakash201@gmail.com>
2025-07-25 19:11:26 +0530

    .

---
+++ Questions/src/LeetCode_Questions/CountAndSay38.java
@@ -0,0 +1,35 @@
+package LeetCode_Questions;
+
+public class CountAndSay38 
+{
+	public static String countAndSay(int n)
+	{
+		if(n==1)
+			return "1";
+
+		String s=countAndSay(n-1);
+
+		StringBuilder result=new StringBuilder();
+
+		int i=0;
+		while(i<s.length())
+		{
+			int count=1;
+
+			while(i+1<s.length() && s.charAt(i)==s.charAt(i+1))
+			{
+				count++;
+				i++;
+			}
+			result=result.append(count).append(s.charAt(i));
+			i++;
+		}
+		return result.toString();
+	}
+	public static void main(String[] args)
+	{
+		int n=4;
+		System.out.println(countAndSay(n));
+	}
+
+}
