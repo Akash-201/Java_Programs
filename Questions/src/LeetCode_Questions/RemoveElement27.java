2bed51da50ab320e5f4c476bf5106a9a5a8f6836
AKASH KUMAR <akakash201@gmail.com>
2025-07-29 18:28:24 +0530

    .

--- Questions/src/LeetCode_Questions/RemoveElement27.java
+++ Questions/src/LeetCode_Questions/RemoveElement27.java
@@ -1,5 +1,7 @@
 package LeetCode_Questions;
 
+//Leetcode: 27. Remove Element
+
 public class RemoveElement27 
 {
 	 public static int removeElement(int[] nums, int val)
@@ -18,8 +20,11 @@ public class RemoveElement27
 	 public static void main(String[] args)
 	 {
 		 int[] nums= {0,1,2,2,3,0,4,2};
+		 int[] nums2= {3,2,2,3};
 		 int val=2;
-		 System.out.println(removeElement(nums, val));
+		 int val2=3;
+		 System.out.println(removeElement(nums, val));   // 5
+		 System.out.println(removeElement(nums2, val2)); // 2
 	 }
 
 }
