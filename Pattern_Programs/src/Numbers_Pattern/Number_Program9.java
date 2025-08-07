3a4f325789c669171e3172d9e72066afe947cb32
AKASH KUMAR <akakash201@gmail.com>
2025-08-07 20:19:07 +0530

    .

--- Pattern_Programs/src/Numbers_Pattern/Number_Program9.java
+++ Pattern_Programs/src/Numbers_Pattern/Number_Program9.java
@@ -1,5 +1,13 @@
 package Numbers_Pattern;
 
+/*
+1 1 2 1 3 1 
+1 2 2 2 3 2 
+1 3 2 3 3 3 
+1 4 2 4 3 4 
+1 5 2 5 3 5
+*/
+
 public class Number_Program9 
 {
 	public static void printPattern(int n, int m)
