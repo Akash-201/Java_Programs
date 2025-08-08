5502e25ab61c868000f676a9ff58238eded0f143
AKASH KUMAR <akakash201@gmail.com>
2025-08-08 18:09:18 +0530

    .

---
+++ Interview_Program/src/CompanyQuestions/NextGreaterElement.java
@@ -0,0 +1,36 @@
+package CompanyQuestions;
+
+import java.util.Arrays;
+
+public class NextGreaterElement 
+{
+	public static void findGreaterElement(int[] arr)
+	{
+		int[] newArr=new int[arr.length];
+
+		for(int i=0;i<arr.length;i++)
+		{
+			boolean found=false;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]<arr[j])
+				{
+					newArr[i]=arr[j];
+					found=true;
+					break;
+				}
+			}
+			if(!found)
+			{
+				newArr[i]=-1;
+			}
+		}
+		System.out.println(Arrays.toString(newArr));
+
+	}
+	public static void main(String[] args) {
+		int[] arr= {2,5,3,1,2,4,6};
+		findGreaterElement(arr);
+	}
+
+}
