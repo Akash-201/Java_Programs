e1a36d7eef31aea545f42216492b523be5917dcf
AKASH KUMAR <akakash201@gmail.com>
2025-08-10 18:58:05 +0530

    .

--- Pattern_Programs/src/Numbers_Pattern/Number_Program11.java
+++ Pattern_Programs/src/Numbers_Pattern/Number_Program11.java
@@ -1,14 +1,22 @@
 package Numbers_Pattern;
 
+/*
+1 2 3 4 5 
+2 3 4 5 6 
+3 4 5 6 7 
+4 5 6 7 8 
+5 6 7 8 9 
+*/
+
 public class Number_Program11 
 {
 	public static void printPattern(int n)
 	{
-		for(int i=0;i<n;i++)
+		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=n;j++)
 			{
-				System.out.print(i+j+" ");
+				System.out.print(i+j-1+" ");
 			}
 			System.out.println();
 		}
