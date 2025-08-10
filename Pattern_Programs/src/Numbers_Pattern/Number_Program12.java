dafbdca2927aea54f52072138dc2346bcfaf34b1
AKASH KUMAR <akakash201@gmail.com>
2025-08-10 19:13:04 +0530

    .

---
+++ Pattern_Programs/src/Numbers_Pattern/Number_Program12.java
@@ -0,0 +1,21 @@
+package Numbers_Pattern;
+
+public class Number_Program12
+{
+	public static void printPattern(int n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print((2*(i+j)-3)+" ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		int n=5;
+		printPattern(n);
+	}
+
+}
