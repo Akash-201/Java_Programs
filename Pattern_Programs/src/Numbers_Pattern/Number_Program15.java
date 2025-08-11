b911ac8f0abc5aed3f1063111895369a274ef10a
AKASH KUMAR <akakash201@gmail.com>
2025-08-11 18:28:51 +0530

    .

--- Pattern_Programs/src/Numbers_Pattern/Number_Program15.java
+++ Pattern_Programs/src/Numbers_Pattern/Number_Program15.java
@@ -15,20 +15,9 @@ public class Number_Program15
 	{
 		for(int i=1;i<=n;i++)
 		{
-			for(int j=1;j<=n;j++)
+			for(int j=0;j<n;j++)
 			{
-				if(i%2==0)
-				{
-					System.out.print(0+" ");
-				}
-				else if((i+j)%2==0)
-				{
-					System.out.print(1+" ");
-				}
-				else
-				{
-					System.out.print(0+" ");
-				}
+				System.out.print((i*j)%2+" ");
 			}
 			System.out.println();
 		}
