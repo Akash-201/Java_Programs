2c9e2685bb0094283d7360e867ce4ee743e920e3
AKASH KUMAR <akakash201@gmail.com>
2025-08-15 17:43:54 +0530

    .

--- Questions/src/LeetCode_Questions/PowerOfFour342.java
+++ Questions/src/LeetCode_Questions/PowerOfFour342.java
@@ -15,7 +15,11 @@ public class PowerOfFour342
 	  }
 	  public static void main(String[] args) {
 		int n=16;
-		System.out.println(isPowerOfFour(n));
+		int n2=5;
+		int n3=1;
+		System.out.println(isPowerOfFour(n));  // true
+		System.out.println(isPowerOfFour(n2)); // false
+		System.out.println(isPowerOfFour(n3)); // true
 	}
 
 }
--- Questions/src/LeetCode_Questions/PowerOfTwo231.java
+++ Questions/src/LeetCode_Questions/PowerOfTwo231.java
@@ -5,7 +5,7 @@ LeetCode: 231. Power of Two
 
 Given an integer n, return true if it is a power of two. Otherwise, return false.
 
-An integer n is a power of two, if there exists an integer x such that n == 2x.
+An integer n is a power of two, if there exists an integer x such that n == 2^x.
 
 Example 1:
 Input: n = 1
