d33b40f66c952246a4cd0b90ba059cb647613fe9
AKASH KUMAR <akakash201@gmail.com>
2025-08-18 20:05:34 +0530

    .

---
+++ Pattern_Programs/src/Numbers_Pattern/FibonacciSeriesPyramidPattern.java
@@ -0,0 +1,26 @@
+package Numbers_Pattern;
+
+public class FibonacciSeriesPyramidPattern
+{
+	public static void printPattern(int n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			int a=0;
+			int b=1;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(a+" ");
+				int c=a+b;
+				a=b;
+				b=c;
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		int n=5;
+		printPattern(n);
+	}
+
+}
