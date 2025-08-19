347fb10ef3cf974ea4f46242b1450dff6def0683
AKASH KUMAR <akakash201@gmail.com>
2025-08-19 18:33:57 +0530

    .

--- Questions/src/LeetCode_Questions/NumberOfZero_FilledSubarrays2348.java
+++ Questions/src/LeetCode_Questions/NumberOfZero_FilledSubarrays2348.java
@@ -1,5 +1,7 @@
 package LeetCode_Questions;
 
+// LeetCode: 2348. Number of Zero-Filled Subarrays
+
 public class NumberOfZero_FilledSubarrays2348 
 {
 	public static long zeroFilledSubarray(int[] nums) 
@@ -21,7 +23,11 @@ public class NumberOfZero_FilledSubarrays2348
 	}
 	public static void main(String[] args) {
 		int[] nums= {1,3,0,0,2,0,0,4};
-		System.out.println(zeroFilledSubarray(nums));
+		int[] nums2= {0,0,0,2,0,0};
+		int[] nums3= {2,10,2019};
+		System.out.println(zeroFilledSubarray(nums));  //6
+		System.out.println(zeroFilledSubarray(nums2)); //9
+		System.out.println(zeroFilledSubarray(nums3)); //0
 		
 	}
 
