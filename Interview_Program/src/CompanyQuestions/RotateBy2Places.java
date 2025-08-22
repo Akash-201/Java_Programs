ee89adf5d7e73751d4c1841040681a4f5263a4d8
AKASH KUMAR <akakash201@gmail.com>
2025-08-22 18:32:32 +0530

    .

--- Interview_Program/src/CompanyQuestions/RotateBy2Places.java
+++ Interview_Program/src/CompanyQuestions/RotateBy2Places.java
@@ -1,12 +1,14 @@
 package CompanyQuestions;
 
+// Given two strings s1 and s2, check if s2 can be obtained by rotating s1 exactly 2 places (either clockwise or anti-clockwise).
+
 public class RotateBy2Places 
 {
 	public static boolean checkContain(String s1, String s2)
 	{
-		String clockWise=s1.substring(2,s1.length())+s1.substring(0,2);
-		String antiClockWise=s1.substring(s1.length()-2,s1.length())+s1.substring(2,s1.length());
-		
+		String clockWise=s1.substring(2)+s1.substring(0,2);
+		String antiClockWise = s1.substring(s1.length() - 2) + s1.substring(0, s1.length() - 2);
+
 		return (s2.equals(antiClockWise) || s2.equals(clockWise));
 	}
 	public static void main(String[] args) {
