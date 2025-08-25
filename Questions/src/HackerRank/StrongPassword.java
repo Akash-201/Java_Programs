b5cc7dcacf92998ffb01755fc88b0a4eaea093e1
AKASH KUMAR <akakash201@gmail.com>
2025-08-25 23:43:39 +0530

    .

--- Questions/src/HackerRank/StrongPassword.java
+++ Questions/src/HackerRank/StrongPassword.java
@@ -3,33 +3,36 @@ package HackerRank;
 public class StrongPassword 
 {
 	public static int minimumNumber(int n, String password) {
-        int missing = 0;
+		int missing = 0;
 
-        boolean hasDigit = false;
-        boolean hasLower = false;
-        boolean hasUpper = false;
-        boolean hasSpecial = false;
+		boolean hasDigit = false;
+		boolean hasLower = false;
+		boolean hasUpper = false;
+		boolean hasSpecial = false;
 
-        for (char ch : password.toCharArray()) {
-            if (Character.isDigit(ch)) hasDigit = true;
-            else if (Character.isLowerCase(ch)) hasLower = true;
-            else if (Character.isUpperCase(ch)) hasUpper = true;
-            else hasSpecial = true; 
-        }
+		for (char ch : password.toCharArray()) {
+			if (Character.isDigit(ch)) hasDigit = true;
+			else if (Character.isLowerCase(ch)) hasLower = true;
+			else if (Character.isUpperCase(ch)) hasUpper = true;
+			else hasSpecial = true; 
+		}
 
-        if (!hasDigit) missing++;
-        if (!hasLower) missing++;
-        if (!hasUpper) missing++;
-        if (!hasSpecial) missing++;
+		if (!hasDigit) missing++;
+		if (!hasLower) missing++;
+		if (!hasUpper) missing++;
+		if (!hasSpecial) missing++;
 
-        return Math.max(missing, 6 - n);
-    }
-	
+		return Math.max(missing, 6 - n);
+	}
+
 	public static void main(String[] args) {
 		String password="2bbbb";
-		int n=6;
-	System.out.println(minimumNumber(n, password));
-		
+		String password2="2bb#A";
+		String password3="#HackerRank";
+		System.out.println(minimumNumber(password.length(), password));  //2
+		System.out.println(minimumNumber(password2.length(), password2)); //1
+		System.out.println(minimumNumber(password3.length(), password3)); //1
+
 	}
 
 }
