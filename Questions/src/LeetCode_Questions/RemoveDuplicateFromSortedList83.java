3016261aa7fda6c9846707ab0a2841ac2611b585
AKASH KUMAR <akakash201@gmail.com>
2025-08-28 18:19:00 +0530

    .

---
+++ Questions/src/LeetCode_Questions/RemoveDuplicateFromSortedList83.java
@@ -0,0 +1,36 @@
+package LeetCode_Questions;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+public class RemoveDuplicateFromSortedList83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) cur.next = cur.next.next;
+            else cur = cur.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        // create list: 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        RemoveDuplicateFromSortedList83 sol = new RemoveDuplicateFromSortedList83();
+        head = sol.deleteDuplicates(head);
+
+        // print result
+        while (head != null) {
+            System.out.print(head.val + (head.next != null ? " -> " : ""));
+            head = head.next;
+        }
+    }
+}
