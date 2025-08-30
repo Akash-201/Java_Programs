0095182798ea8109a3823b0180477f40739c3a6d
AKASH KUMAR <akakash201@gmail.com>
2025-08-30 18:10:25 +0530

    .

---
+++ Questions/src/HackerRank/BirthdayCakeCandles.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BirthdayCakeCandles
+{
+	public static int birthdayCakeCandles(List<Integer> candles)
+	{
+		int tallest=Integer.MIN_VALUE;
+		int count=0;
+		for(int height : candles)
+		{
+			if(height > tallest)
+			{
+				tallest=height;
+				count=1;
+			}
+			else if(height == tallest)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args)
+	{
+		List<Integer> candles=Arrays.asList(4,4,1,3);
+		List<Integer> candles2=Arrays.asList(3,2,3,1,3);
+		
+		System.out.println(birthdayCakeCandles(candles));  // 2
+		System.out.println(birthdayCakeCandles(candles2)); // 3
+		
+	}
+
+}
