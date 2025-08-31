ebb3c25d00d4865c0d2f918dc6728f8843b19d1e
AKASH KUMAR <akakash201@gmail.com>
2025-08-31 19:11:21 +0530

    .

---
+++ Questions/src/HackerRank/TimeConversion.java
@@ -0,0 +1,27 @@
+package HackerRank;
+
+public class TimeConversion
+{
+	public static String timeConversion(String s) {
+		String str="";
+		if(s.substring(0,2).equals("12") && s.contains("AM"))
+		{
+			str="00";
+			s=str+s.substring(2);
+		}
+
+		if(!s.substring(0,2).equals("12") && s.contains("PM"))
+		{
+			str=Integer.toString(Integer.parseInt(s.substring(0,2))+12);
+			s=str+s.substring(2);
+		}
+		return s.substring(0,s.length()-2);
+
+	}
+	public static void main(String[] args) {
+		String s="12:01:00AM";
+		System.out.println(timeConversion(s));
+	}
+
+
+}
