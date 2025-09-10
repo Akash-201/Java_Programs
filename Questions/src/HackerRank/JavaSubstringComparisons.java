c6d3b217774d339438d6374230edb72f52f634e3
AKASH KUMAR <akakash201@gmail.com>
2025-09-10 17:32:29 +0530

    .

--- Questions/src/HackerRank/JavaSubstringComparisons.java
+++ Questions/src/HackerRank/JavaSubstringComparisons.java
@@ -9,12 +9,15 @@ public class JavaSubstringComparisons
 		String smallest = s.substring(0, k);
 		String largest = s.substring(0, k);
 
-		for (int i = 1; i <= s.length() - k; i++) {
+		for (int i = 1; i <= s.length() - k; i++) 
+		{
 			String currentSubstring = s.substring(i, i + k);
-			if (currentSubstring.compareTo(smallest) < 0) {
+			if (currentSubstring.compareTo(smallest) < 0) 
+			{
 				smallest = currentSubstring;
 			}
-			if (currentSubstring.compareTo(largest) > 0) {
+			if (currentSubstring.compareTo(largest) > 0) 
+			{
 				largest = currentSubstring;
 			}
 		}
@@ -22,13 +25,14 @@ public class JavaSubstringComparisons
 		return smallest + "\n" + largest;
 	}
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter s: ");
-		String s = scan.next();
+		String s = sc.next();
 
 		System.out.print("Enter k: ");
-		int k = scan.nextInt();
+		int k = sc.nextInt();
 
 		System.out.println(getSmallestAndLargest(s, k));
 	}
