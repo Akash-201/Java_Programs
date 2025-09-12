17c4c6b6157bdbc9659f35d34ee0d07bd4de7e7f
AKASH KUMAR <akakash201@gmail.com>
2025-09-12 18:09:41 +0530

    .

--- Questions/src/LeetCode_Questions/VowelsGameInA_String3227.java
+++ Questions/src/LeetCode_Questions/VowelsGameInA_String3227.java
@@ -1,23 +1,26 @@
 package LeetCode_Questions;
 
+// Leetcode: 3227. Vowels Game in a String
+
 public class VowelsGameInA_String3227 
 {
 	public static boolean doesAliceWin(String s) 
-    {
-        for(int i=0;i<s.length();i++)
-        {
-            char c =s.charAt(i);
-            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' )
-            {
-                return true;
-            }
-
-        }
-        return false;
-        
-    }
-	public static void main(String[] args) {
+	{
+		for(int i=0;i<s.length();i++)
+		{
+			char c =s.charAt(i);
+			if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' )
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args) 
+	{
 		String s="leetcoder";
-		System.out.println(doesAliceWin(s));
+		String s2="bbcd";
+		System.out.println(doesAliceWin(s));  // true
+		System.out.println(doesAliceWin(s2)); // false
 	}
 }
