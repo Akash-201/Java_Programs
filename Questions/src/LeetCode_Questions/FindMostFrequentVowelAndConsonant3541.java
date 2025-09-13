fa196dac2aca6e942a6c0642fec06941699c3b1c
AKASH KUMAR <akakash201@gmail.com>
2025-09-13 18:33:42 +0530

    .

--- Questions/src/LeetCode_Questions/FindMostFrequentVowelAndConsonant3541.java
+++ Questions/src/LeetCode_Questions/FindMostFrequentVowelAndConsonant3541.java
@@ -1,5 +1,7 @@
 package LeetCode_Questions;
 
+// Leetcode: 3541. Find Most Frequent Vowel and Consonant
+
 public class FindMostFrequentVowelAndConsonant3541 
 {
 	public static int maxFreqSum(String s) 
@@ -15,15 +17,21 @@ public class FindMostFrequentVowelAndConsonant3541
 			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
 			{
 				mv = Math.max(mv, freq[c-'a']);
-			}else{
+			}
+			else
+			{
 				mc = Math.max(mc, freq[c-'a']);
 			}
 		}
 		return mv + mc;
 	}
-public static void main(String[] args) {
-	String s="successes";
-	System.out.println(maxFreqSum(s));
-}
+	public static void main(String[] args) 
+	{
+		String s="successes";
+		String s2="aeiaeia";
+
+		System.out.println(maxFreqSum(s));  // 6
+		System.out.println(maxFreqSum(s2)); // 3
+	}
 
 }
