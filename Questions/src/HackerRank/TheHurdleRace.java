2ed515379762b933e6299e872b6aaa1e8da7d658
AKASH KUMAR <akakash201@gmail.com>
2025-09-14 23:04:58 +0530

    .

---
+++ Questions/src/HackerRank/TheHurdleRace.java
@@ -0,0 +1,25 @@
+package HackerRank;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TheHurdleRace 
+{
+	public static int hurdleRace(int k, List<Integer> height) 
+	{
+		int maxHeight = Collections.max(height);
+		int potionsNeeded = maxHeight - k;
+		return Math.max(potionsNeeded, 0);
+
+	}
+	public static void main(String[] args)
+	{
+		int k=4;
+		List<Integer>height=Arrays.asList(1,6,3,5,2);
+		System.out.println(hurdleRace(k, height));
+
+
+	}
+
+}
