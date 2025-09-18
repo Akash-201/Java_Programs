0fabe5ba1922f8eada00b116dc0bf867eb80e382
AKASH KUMAR <akakash201@gmail.com>
2025-09-18 12:09:12 +0530

    .

--- Questions/src/HackerRank/JumpingOnTheCloudsRevisited.java
+++ Questions/src/HackerRank/JumpingOnTheCloudsRevisited.java
@@ -8,27 +8,33 @@ public class JumpingOnTheCloudsRevisited
 		int energy = 100;
 		int currentPosition = 0;
 
-		while (true) {
-
+		while (true) 
+		{
 			currentPosition  = (currentPosition + k) % n;
 			energy--;
 
-			if (c[currentPosition] == 1) {
+			if (c[currentPosition] == 1) 
+			{
 				energy -= 2;
 			}
 
-
-			if (currentPosition == 0) {
+			if (currentPosition == 0) 
+			{
 				break;
 			}
 		}
 
 		return energy;
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int[] c= {0, 0, 1, 0, 0, 1, 1, 0};
 		int k=2;
-		System.out.println(jumpingOnClouds(c, k));
+		System.out.println(jumpingOnClouds(c, k));  // 92
+		
+		int[] c2= {0,0,1,0};
+		int k2=2;
+		System.out.println(jumpingOnClouds(c2, k2)); // 96
 	}
 
 }
