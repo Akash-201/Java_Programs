d7d75e1b149e271064621a38a519df4d61077929
AKASH KUMAR <akakash201@gmail.com>
2025-09-19 18:10:34 +0530

    .

---
+++ Questions/src/HackerRank/AppendAndDelete.java
@@ -0,0 +1,42 @@
+package HackerRank;
+
+public class AppendAndDelete 
+{
+	public static String appendAndDelete(String s, String t, int k) {
+		int commonLength = 0;
+
+		// Find common prefix length
+		for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+			if (s.charAt(i) == t.charAt(i)) {
+				commonLength++;
+			} else {
+				break;
+			}
+		}
+
+		int deletions = s.length() - commonLength;
+		int appends = t.length() - commonLength;
+		int totalOps = deletions + appends;
+
+		//  If we have enough ops to delete entire string and rebuild
+		if (k >= s.length() + t.length()) {
+			return "Yes";
+		}
+
+		//  If exact match possible
+		if (totalOps <= k && (k - totalOps) % 2 == 0) {
+			return "Yes";
+		}
+
+		return "No";
+	}
+	public static void main(String[] args) {
+		String s="hackerhappy";
+		String t="hackerrank";
+		int k=9;
+		System.out.println(appendAndDelete(s, t, k));
+
+	}
+
+
+}
