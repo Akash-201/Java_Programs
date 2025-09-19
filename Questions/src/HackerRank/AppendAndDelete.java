36f49f6dbfc361b6052d70ba45ad0e012d8f9a1f
AKASH KUMAR <akakash201@gmail.com>
2025-09-19 18:13:27 +0530

    .

--- Questions/src/HackerRank/AppendAndDelete.java
+++ Questions/src/HackerRank/AppendAndDelete.java
@@ -2,14 +2,19 @@ package HackerRank;
 
 public class AppendAndDelete 
 {
-	public static String appendAndDelete(String s, String t, int k) {
+	public static String appendAndDelete(String s, String t, int k)
+	{
 		int commonLength = 0;
 
 		// Find common prefix length
-		for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
-			if (s.charAt(i) == t.charAt(i)) {
+		for (int i = 0; i < Math.min(s.length(), t.length()); i++)
+		{
+			if (s.charAt(i) == t.charAt(i))
+			{
 				commonLength++;
-			} else {
+			} 
+			else
+			{
 				break;
 			}
 		}
@@ -19,24 +24,23 @@ public class AppendAndDelete
 		int totalOps = deletions + appends;
 
 		//  If we have enough ops to delete entire string and rebuild
-		if (k >= s.length() + t.length()) {
+		if (k >= s.length() + t.length())
+		{
 			return "Yes";
 		}
 
 		//  If exact match possible
-		if (totalOps <= k && (k - totalOps) % 2 == 0) {
+		if (totalOps <= k && (k - totalOps) % 2 == 0) 
+		{
 			return "Yes";
 		}
 
 		return "No";
 	}
-	public static void main(String[] args) {
-		String s="hackerhappy";
-		String t="hackerrank";
-		int k=9;
-		System.out.println(appendAndDelete(s, t, k));
-
+	public static void main(String[] args) 
+	{
+		System.out.println(appendAndDelete("hackerhappy","hackerrank",9));  // Yes
+		System.out.println(appendAndDelete("aba", "aba",7));  // Yes
+		System.out.println(appendAndDelete("ashley", "ash",2)); // No
 	}
-
-
 }
