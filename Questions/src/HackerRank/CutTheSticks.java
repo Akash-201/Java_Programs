3e9f3be22795c56553977dcb98a6d8064655ed3d
AKASH KUMAR <akakash201@gmail.com>
2025-09-20 21:32:24 +0530

    .

--- Questions/src/HackerRank/CutTheSticks.java
+++ Questions/src/HackerRank/CutTheSticks.java
@@ -7,33 +7,41 @@ import java.util.List;
 
 public class CutTheSticks 
 {
-	public static List<Integer> cutTheSticks(List<Integer> arr) {
-	    List<Integer> result = new ArrayList<>();
-	    
-	    while (!arr.isEmpty()) {
-	        // add current stick count
-	        result.add(arr.size());
-	        
-	        // find minimum stick
-	        int min = Collections.min(arr);
-	        
-	        // subtract min from all
-	        List<Integer> newArr = new ArrayList<>();
-	        for (int length : arr) {
-	            int newLength = length - min;
-	            if (newLength > 0) {
-	                newArr.add(newLength);//keep only sticks still > 0
-	            }
-	        }
-	        arr = newArr; // update list
-	    }
-	    return result;
+	public static List<Integer> cutTheSticks(List<Integer> arr) 
+	{
+		List<Integer> result = new ArrayList<>();
+
+		while (!arr.isEmpty()) 
+		{
+			// add current stick count
+			result.add(arr.size());
+
+			// find minimum stick
+			int min = Collections.min(arr);
+
+			// subtract min from all
+			List<Integer> newArr = new ArrayList<>();
+			for (int length : arr) 
+			{
+				int newLength = length - min;
+				if (newLength > 0) 
+				{
+					newArr.add(newLength);//keep only sticks still > 0
+				}
+			}
+			arr = newArr; // update list
+		}
+		return result;
 	}
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		List<Integer> arr=Arrays.asList(5, 4, 4, 2, 2, 8);
-		System.out.println(cutTheSticks(arr));
+		List<Integer> arr2=Arrays.asList(1, 2, 3, 4, 3, 3, 2, 1);
+
+		System.out.println(cutTheSticks(arr));  // [6, 4, 2, 1]
+		System.out.println(cutTheSticks(arr2)); // [8, 6, 4, 1]
 	}
 
 }
