5fe60dbf8fd15235aeb43fcfddaeb88c6ea31135
AKASH KUMAR <akakash201@gmail.com>
2025-09-21 19:12:33 +0530

    .

---
+++ Questions/src/HackerRank/JumpingOnTheClouds.java
@@ -0,0 +1,30 @@
+package HackerRank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JumpingOnTheClouds
+{
+	public static int jumpingOnClouds(List<Integer> c) {
+        int jumps = 0;
+        int i = 0;
+
+        while (i < c.size() - 1) {
+            // if we can jump 2 steps safely, do it
+            if (i + 2 < c.size() && c.get(i + 2) == 0) {
+                i += 2;
+            } else {
+                i += 1;
+            }
+            jumps++;
+        }
+
+        return jumps;
+    }
+
+	public static void main(String[] args) {
+		List<Integer> c=Arrays.asList(0, 0, 1, 0, 0, 1, 0);
+		System.out.println(jumpingOnClouds(c));
+	}
+
+}
