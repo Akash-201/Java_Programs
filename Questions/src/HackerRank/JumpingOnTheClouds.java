f7783c67f800cad285d866ce72fe158ae9fb5fe4
AKASH KUMAR <akakash201@gmail.com>
2025-09-21 19:14:35 +0530

    .

--- Questions/src/HackerRank/JumpingOnTheClouds.java
+++ Questions/src/HackerRank/JumpingOnTheClouds.java
@@ -5,26 +5,35 @@ import java.util.List;
 
 public class JumpingOnTheClouds
 {
-	public static int jumpingOnClouds(List<Integer> c) {
-        int jumps = 0;
-        int i = 0;
+	public static int jumpingOnClouds(List<Integer> c)
+	{
+		int jumps = 0;
+		int i = 0;
 
-        while (i < c.size() - 1) {
-            // if we can jump 2 steps safely, do it
-            if (i + 2 < c.size() && c.get(i + 2) == 0) {
-                i += 2;
-            } else {
-                i += 1;
-            }
-            jumps++;
-        }
+		while (i < c.size() - 1) 
+		{
+			// if we can jump 2 steps safely, do it
+			if (i + 2 < c.size() && c.get(i + 2) == 0)
+			{
+				i += 2;
+			}
+			else 
+			{
+				i += 1;
+			}
+			jumps++;
+		}
 
-        return jumps;
-    }
+		return jumps;
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		List<Integer> c=Arrays.asList(0, 0, 1, 0, 0, 1, 0);
-		System.out.println(jumpingOnClouds(c));
+		System.out.println(jumpingOnClouds(c));  // 4
+
+		List<Integer> c2=Arrays.asList(0, 0, 0, 0, 1, 0);
+		System.out.println(jumpingOnClouds(c2)); // 3
 	}
 
 }
