7b00ff2c3dfbefb9712fecc93568806058592f8d
AKASH KUMAR <akakash201@gmail.com>
2025-09-26 15:46:20 +0530

    .

--- Questions/src/LeetCode_Questions/ValidTriangleNumber611.java
+++ Questions/src/LeetCode_Questions/ValidTriangleNumber611.java
@@ -1,38 +1,43 @@
 package LeetCode_Questions;
 
+// LeetCode: 611. Valid Triangle Number
+
 import java.util.Arrays;
 
 public class ValidTriangleNumber611 
 {
-	 public static int triangleNumber(int[] nums) 
-	    {
-	        Arrays.sort(nums);
-	        int count=0;
-	        int n=nums.length;
-	        for(int c=n-1;c>=2;c--)
-	        {
-	            int a=0,b=c-1;
+	public static int triangleNumber(int[] nums) 
+	{
+		Arrays.sort(nums);
+		int count=0;
+		int n=nums.length;
+		for(int c=n-1;c>=2;c--)
+		{
+			int a=0,b=c-1;
 
-	            while(a<b)
-	            {
-	                if(nums[a]+nums[b]>nums[c])
-	                {
-	                    count+=(b-a);
-	                    b-=1;
-	                }
-	                else
-	                {
-	                    a+=1;
-	                }
-	            }
-	        }
-	        return count;
-	        
-	    }
-	 public static void main(String[] args) {
+			while(a<b)
+			{
+				if(nums[a]+nums[b]>nums[c])
+				{
+					count+=(b-a);
+					b-=1;
+				}
+				else
+				{
+					a+=1;
+				}
+			}
+		}
+		return count;
+
+	}
+	public static void main(String[] args)
+	{
 		int[] nums= {2,2,3,4};
-		System.out.println(triangleNumber(nums));
-		
+		int[] nums2= {4,2,3,4};
+		System.out.println(triangleNumber(nums));  // 3
+		System.out.println(triangleNumber(nums2)); // 4
+
 	}
 
 }
