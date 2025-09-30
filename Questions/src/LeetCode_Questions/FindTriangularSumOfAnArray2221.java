c20a4bbfd3bfb43071674dd9e300e54a8611fd5e
AKASH KUMAR <akakash201@gmail.com>
2025-09-30 17:30:08 +0530

    .

--- Questions/src/LeetCode_Questions/FindTriangularSumOfAnArray2221.java
+++ Questions/src/LeetCode_Questions/FindTriangularSumOfAnArray2221.java
@@ -1,23 +1,29 @@
 package LeetCode_Questions;
 
+// LeetCode: 2221. Find Triangular Sum of an Array
+
 public class FindTriangularSumOfAnArray2221 
 {
 	public static  int triangularSum(int[] nums)
-    {
-        int n=nums.length;
-        for(int len=n;len>1;len--)
-        {
-            for(int i=0;i<len-1;i++)
-            {
-                nums[i]=(nums[i]+nums[i+1])%10;
-            }
-        }
-        return nums[0];
-        
-    }
-	public static void main(String[] args) {
+	{
+		int n=nums.length;
+		for(int len=n;len>1;len--)
+		{
+			for(int i=0;i<len-1;i++)
+			{
+				nums[i]=(nums[i]+nums[i+1])%10;
+			}
+		}
+		return nums[0];
+
+	}
+	public static void main(String[] args) 
+	{
 		int[] nums= {1,2,3,4,5};
-		System.out.println(triangularSum(nums));
+		System.out.println(triangularSum(nums));  // 8
+
+		int[] nums2= {5};
+		System.out.println(triangularSum(nums2)); // 5
 	}
 
 }
