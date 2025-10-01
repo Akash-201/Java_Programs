cfc545e1c0178e3989a2c25f63ebddca0e8324ef
AKASH KUMAR <akakash201@gmail.com>
2025-10-01 16:26:31 +0530

    .

--- Questions/src/LeetCode_Questions/WaterBottles1518.java
+++ Questions/src/LeetCode_Questions/WaterBottles1518.java
@@ -1,25 +1,28 @@
 package LeetCode_Questions;
 
+// LeetCode: 1518. Water Bottles
+
 public class WaterBottles1518 
 {
 	public static int numWaterBottles(int numBottles, int numExchange)
-    {
-         int result = numBottles; 
-        int emptyWB = numBottles;
+	{
+		int result = numBottles; 
+		int emptyWB = numBottles;
 
-        while (emptyWB >= numExchange) {
-          
-            int newWB = emptyWB / numExchange; 
-            result += newWB;
-            emptyWB = newWB + (emptyWB % numExchange);
-        }
+		while (emptyWB >= numExchange) 
+		{
 
-        return result;
-        
-    }
-	public static void main(String[] args) {
-		System.out.println(numWaterBottles(9,3));
-		
+			int newWB = emptyWB / numExchange; 
+			result += newWB;
+			emptyWB = newWB + (emptyWB % numExchange);
+		}
+		return result;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(numWaterBottles(9,3));  // 13
+		System.out.println(numWaterBottles(15,4)); // 19
+
 	}
 
 }
