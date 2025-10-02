f864df6210d01e16ef112755c8605ddf8fff57c6
AKASH KUMAR <akakash201@gmail.com>
2025-10-02 16:55:05 +0530

    .

--- Questions/src/LeetCode_Questions/WaterBottlesII3100.java
+++ Questions/src/LeetCode_Questions/WaterBottlesII3100.java
@@ -1,22 +1,28 @@
 package LeetCode_Questions;
 
+// LeetCode: 3100. Water Bottles II
+
 public class WaterBottlesII3100
 {
-	public static int maxBottlesDrunk(int numBottles, int numExchange) {
-        int result = numBottles;
-       int emptyWB = numBottles;
+	public static int maxBottlesDrunk(int numBottles, int numExchange) 
+	{
+		int result = numBottles;
+		int emptyWB = numBottles;
 
-       while (emptyWB >= numExchange) {
-           emptyWB = emptyWB - numExchange;
-           numExchange += 1;
-           result += 1;
-           emptyWB += 1;
-       }
+		while (emptyWB >= numExchange) 
+		{
+			emptyWB = emptyWB - numExchange;
+			numExchange += 1;
+			result += 1;
+			emptyWB += 1;
+		}
 
-       return result;
-   } 
-	public static void main(String[] args) {
-		System.out.println(maxBottlesDrunk(13,6));
+		return result;
+	} 
+	public static void main(String[] args)
+	{
+		System.out.println(maxBottlesDrunk(13,6));  // 15
+		System.out.println(maxBottlesDrunk(10,3));  // 13
 	}
 
 }
