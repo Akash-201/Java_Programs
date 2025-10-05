e8b5a02088c5af46a769c38316dbf5119bbad6f4
AKASH KUMAR <akakash201@gmail.com>
2025-10-05 15:50:18 +0530

    .

---
+++ Questions/src/HackerRank/JavaLoopsII.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaLoopsII 
+{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int n = in.nextInt();
+
+			int sum = a;
+			for (int j = 0; j < n; j++)
+			{
+				sum += b * Math.pow(2, j);  
+				System.out.print(sum);
+
+				if (j != n - 1) 
+				{           
+
+					System.out.print(" ");
+
+				}
+			}
+			System.out.println();
+		}
+		in.close();
+
+	}
+
+}
