2772b0ddfc015a61fee7991468769d082ef837b1
AKASH KUMAR <akakash201@gmail.com>
2025-11-25 07:55:40 +0530

    .

---
+++ Questions/src/HackerRank/FlippingBits.java
@@ -0,0 +1,16 @@
+package HackerRank;
+
+public class FlippingBits 
+{
+	public static long flippingBits(long n) 
+	{
+		return 4294967295L - n;
+
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(flippingBits(1));
+		
+	}
+
+}
