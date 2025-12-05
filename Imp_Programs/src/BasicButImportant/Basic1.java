f74c04c083b9dfea3aa740a849c2dfc5557f3111
AKASH KUMAR <akakash201@gmail.com>
2025-12-05 18:34:00 +0530

    .

---
+++ Imp_Programs/src/BasicButImportant/Basic1.java
@@ -0,0 +1,16 @@
+package BasicButImportant;
+
+public class Basic1 
+{
+	public static void main(String[] args) 
+	{
+		int a=0,b=10;
+		if(a!=0 && b/a>1)
+		{
+			System.out.println("True");
+		}
+		System.out.println("False");
+		
+	}
+
+}
