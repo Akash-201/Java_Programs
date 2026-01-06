c68832edff7986e885b388fcd443307afe063f6a
AKASH KUMAR <akakash201@gmail.com>
2026-01-06 12:08:29 +0530

    .

---
+++ PracticeProgram/src/MultiThreading/YieldMethod.java
@@ -0,0 +1,23 @@
+package MultiThreading;
+
+public class YieldMethod extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(Thread.currentThread().getName()+"-"+i);
+		}
+	}
+	public static void main(String[] args) {
+		YieldMethod ym=new YieldMethod();
+		ym.start();
+
+		for(int i=1;i<=5;i++)
+		{
+			Thread.yield();
+			System.out.println("Main Thread"+"-"+i);
+		}
+	}
+
+}
